04222b71ff2753b5e3e5f5b1598f2281181b8a7c
Fagner_0 <ferreirabagundes@gmail.com>
2023-11-08 16:35:21 -0300

    aprendendo sobre Math

---
+++ exercicios/ex014/index.js
@@ -0,0 +1,11 @@
+let num1 = 25;
+
+console.log(100/0);
+// console.log(num1 ** 0.5);
+
+// let nume2 = Math.florr(num1);
+// let num2 = Math.ceil(num1);
+// let num2 = Math.round(num1)
+// console.log(Math.min(1, 3, 6, 4, 100, 430940, 323,)); 
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+
