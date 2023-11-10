72e83321662cc808606652052d00f9e43fd22f06
Fagner_0 <ferreirabagundes@gmail.com>
2023-11-10 15:14:37 -0300

    exercícios com Number e Math

--- atividades/ativ06/index.js
+++ atividades/ativ06/index.js
@@ -1,5 +1,38 @@
-let num = prompt(`Digite um numeo:`)
-const primeiro = document.getElementById(`titulo`)
-const texto = document.getElementById(`texto`)
+let numero = Number(prompt(`Digite um número:`))
 
-primeiro.innerHTML = `${num}`
+let titulo = document.getElementById(`numeroTitulo`)
+
+titulo.innerHTML += `${numero}`
+
+let texto = document.getElementById(`texto`)
+
+texto.innerHTML += `
+<p> 
+  Raiz quadrada do seu numero: ${numero ** 0.5}
+</p>
+
+<p>
+  O seu numero é inteiro?: ${Number.isInteger(numero)}
+</p>
+
+<p>
+  O seu número é NaN?: ${Number.isNaN(numero)}
+</p>
+
+<p> 
+  Seu número arredondado para baixo: ${Math.floor(numero)}
+</p>
+
+<p>
+  Seu número arredondado para cima: ${Math.ceil(numero)}
+</p>
+
+<p> 
+  Seu número com duas casa decimais: ${numero.toFixed(2)}
+</p>
+
+`
+
+//  para pegar a raiz quadrada, basta fazer: valor ** 0.5
+
+
