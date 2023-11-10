9fbdb732ad5fdf9de5ec015cdec1d6ccd798a6d3
Fagner_0 <ferreirabagundes@gmail.com>
2023-11-10 19:01:50 -0300

    .

---
+++ exercicios/ex015/index.js
@@ -0,0 +1,29 @@
+const alunos = [`Maria`, `Fagner`, `Eduardo`];
+
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+
+// alunos.push(`Edinaldo`);
+// alunos.push(`Edevaldo`);
+
+// console.log(typeof alunos)
+
+// console.log(alunos.slice(0, -3));
+// alunos.shift(); // revome do começo
+
+// alunos.pop(); // remove do final
+
+// alunos.unshift(`Dylon`) // adiciona no começo e muda os índices de lugar
+
+// alunos.push(`Gabriel`) //adiciona no final
+
+// alunos[alunos.length] = `Adalberto` // adiciona no final
+
+// console.log(alunos.length);
+// alunos[0] = `Fabrício`
+// alunos[3] = `Luzia`
+// console.log(alunos);
+// console.log(alunos[2]);
+
