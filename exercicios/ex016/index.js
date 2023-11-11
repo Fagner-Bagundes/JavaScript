a7bc2cfefcd4b691949322911bd058ec60ef2175
Fagner_0 <ferreirabagundes@gmail.com>
2023-11-11 14:47:57 -0300

    aprendendo sobre Funções

---
+++ exercicios/ex016/index.js
@@ -0,0 +1,45 @@
+/*
+
+PRIMEIRA MANAEIRA DE CRIAR UMA FUNÇÃO;
+
+ function soma(x, y){
+    const resultado = x + y;
+     return resultado; 
+}
+
+ console.log(soma(2, 2));
+
+*/
+
+
+/*
+
+SEGUNDA MANEIRA DE CRIAR UMA FUNÇÃO:
+
+const raiz  = function (n) {
+    return n ** 0.5;
+};
+
+console.log(raiz(25));
+console.log(raiz(16));
+console.log(raiz(25));
+
+*/
+
+/* 
+
+TERCEIRA MANEIRA DE CRIAR UMA FUNÇÃO:
+
+ Nesse caso, a difereça é que vc não usa a palavra FUNCTION. você substitui por um simbulo de:
+ 
+ =>
+
+ ex:
+
+ const raiz = (n) => {
+    return n ** 0.5;
+ };
+
+ console.log(raiz(9));
+ 
+*/
