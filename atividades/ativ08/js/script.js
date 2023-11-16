1028e9b8cb12036a3ef949ec815b28ad2d044c52
Fagner_0 <ferreirabagundes@gmail.com>
2023-11-16 15:59:29 -0300

    terminado

--- atividades/ativ08/js/script.js
+++ atividades/ativ08/js/script.js
@@ -24,23 +24,23 @@ function recebeEvento(evento){
         resposta.innerHTML = `você tá a baixo do peso. tá passando fome? tá só o palitoKKKKK`
     }
     else if (resultado>= 18.5 && resultado <= 24.9) {
-        resposta.innerHTML= ` você tá no peso ideal, agora só falta parar de dar esse cu aikkkk`;
+        resposta.innerHTML= ` Você tá no peso ideal, agora só falta parar de dar esse cu aikkkk`;
     }
     else if (resultado >= 25 && resultado <= 29.9) {
-        resposta.innerHTML= `você está com sobrepeso. Tá gordo man, bolotinhakkk`;
+        resposta.innerHTML= `Você está com sobrepeso. Tá gordo man, bolotinhakkk`;
     }
     else if (resultado >= 30 && resultado <= 34.9) {
-        resposta.innerHTML= `você tá com obesidade. tu tá muito gordo veio, para de comer manokkkk`;
+        resposta.innerHTML= `Você tá com obesidade. tu tá muito gordo veio, para de comer manokkkk`;
     }
     else if (resultado >=35 && resultado <= 39.9){
-        resposta.innerHTML= `tu tá com obesidade severa velho, para de comer mano:(`;
+        resposta.innerHTML= `Você tá com obesidade severa velho, para de comer mano:(`;
 
     }
     else if (resultado > 40) {
-        resposta.innerHTML= `tá comendo toda a comida do mundo seu fdp? poha vei, tu vai morrer mano. pessoal quer comer tbm carakkk. Medo.`;
+        resposta.innerHTML= `Tá comendo toda a comida do mundo seu fdp? poha vei, tu vai morrer mano. pessoal quer comer tbm carakkk. Medo.`;
     }
     else{
-        resposta.innerHTML = `tu tem que colocar alguma coisa seu gay, acha que tu aqui pra zoar??`;
+        resposta.innerHTML = `Você tem que colocar alguma coisa seu gay, acha que tô aqui pra brincadeira??`;
         
     }
 }
