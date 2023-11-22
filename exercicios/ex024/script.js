6fa6954261a912b627eb3ac623ab9ae5fa32ee69
Fagner_0 <ferreirabagundes@gmail.com>
2023-11-22 17:49:20 -0300

    aprendendo sobre switch/case

---
+++ exercicios/ex024/script.js
@@ -0,0 +1,69 @@
+const data = new Date();
+const diaSemana = data.getDay() + 1;
+let diaSemanaTexto;
+
+function getDiaSemanaTexto(diaSemana) {
+    switch (diaSemana) {
+        case 0:
+            diaSemanaTexto = `Domingo`
+            return diaSemanaTexto;
+        case 1:
+            diaSemanaTexto = `Segunda`
+            return diaSemanaTexto;
+        case 2:
+            diaSemanaTexto = `Terça`
+            return diaSemanaTexto;
+        case 3:
+            diaSemanaTexto = `Quarta`
+            return diaSemanaTexto;
+        case 4: 
+        diaSemanaTexto = `Quinta`
+            return diaSemanaTexto;
+        case 5:
+            diaSemanaTexto = `Sexta`
+            return diaSemanaTexto;
+        case 6:
+            diaSemanaTexto = `Sabado`
+            return diaSemanaTexto;
+        default:
+            diaSemanaTexto = ``
+            return diaSemanaTexto;
+    }
+    
+}
+ diaSemanaTexto = getDiaSemanaTexto(diaSemana);
+console.log(diaSemanaTexto);
+
+
+
+
+
+
+
+
+// if (diaSemana === 0) {
+//     diaSemanaTexto = `Domingo`
+    
+// } else if(diaSemana === 1){
+//     diaSemanaTexto = `Segunda`
+// }
+// if (diaSemana === 2) {
+//     diaSemanaTexto = `Terça`
+    
+// } else if(diaSemana === 3){
+//     diaSemanaTexto = `Quarta`
+// }
+
+// if (diaSemana === 4) {
+//     diaSemanaTexto = `Quinta`
+    
+// } else if(diaSemana === 5){
+//     diaSemanaTexto = `Sexta`
+// }
+
+// if (diaSemana === 6) {
+//     diaSemanaTexto = `Sabado`
+// }
+// console.log(diaSemana);
+// console.log(diaSemanaTexto);
+
--- treino/tr1.js
+++ treino/tr1.js
@@ -1,3 +1,23 @@
-const nome = `Fagner`;
-let sobrenome = `Ferreira`;
-console.log(`salva salve familia, meu nome é ${nome} ${sobrenome}`);
+
+function diaSemanaTexto(sla) {
+    
+}
+let pontuacaoUsuario = 1;
+let nivelUsuario;
+
+switch(pontuacaoUsuario){
+    case 1:
+        nivelUsuario = `Nivel Bronze`
+        break;
+    case 2:
+        nivelUsuario = `Nível prata`
+        break;
+    case 3: 
+        nivelUsuario = `Nível Ouro`
+        break;
+    default:
+        nivelUsuario = ``
+}
+console.log(nivelUsuario);
+
+
