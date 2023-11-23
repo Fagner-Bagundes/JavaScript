9c4b15f5be0e8b99f9bb74ad2e51582ed46f0cfb
Fagner_0 <ferreirabagundes@gmail.com>
2023-11-23 14:37:50 -0300

    .

--- atividades/ativ09/assets/js/main.js
+++ atividades/ativ09/assets/js/main.js
@@ -1,103 +1,110 @@
-function global() {
-    const data = new Date();
-    let diaEmTexto;
-    let mesEmtexto;
+// function global() {
+//     const data = new Date();
+//     let diaEmTexto;
+//     let mesEmtexto;
 
-    function formatadorData(valor) {
-        const dia = data.getDate();
-        const mes = data.getMonth();
-        const ano = data.getFullYear();
-        const hora = data.getHours();
-        const minutos = data.getMinutes();
-        const diaSemana = data.getDay();
+//     function formatadorData(valor) {
+//         const dia = data.getDate();
+//         const mes = data.getMonth();
+//         const ano = data.getFullYear();
+//         const hora = data.getHours();
+//         const minutos = data.getMinutes();
+//         const diaSemana = data.getDay();
         
-        return [dia, mes, ano, hora, minutos, diaSemana]
-    }
-const novaData = formatadorData();
+//         return [dia, mes, ano, hora, minutos, diaSemana]
+//     }
+// const novaData = formatadorData();
 
 
-function getDiaEmTexto(diaSemana) {
-    switch (novaData[5]) {
-        case 0:
-            diaEmTexto = `Domingo`
-            return diaEmTexto;
-        case 1:
-            diaEmTexto = `Segunda-feira`
-            return diaEmTexto;
-        case 2:
-            diaEmTexto = `Terça-feira`
-            return diaEmTexto;
-        case 3:
-            diaEmTexto = `Quarta-feira`
-            return diaEmTexto;
-        case 4: 
-        diaEmTexto = `Quinta-feira`
-            return diaEmTexto;
-        case 5:
-            diaEmTexto = `Sexta-feira`
-            return diaEmTexto;
-        case 6:
-            diaEmTexto = `Sabado`
-            return diaEmTexto;
-        default:
-            diaEmTexto = ``
-            return diaEmTexto;
-    }
+// function getDiaEmTexto(diaSemana) {
+//     switch (novaData[5]) {
+//         case 0:
+//             diaEmTexto = `Domingo`
+//             return diaEmTexto;
+//         case 1:
+//             diaEmTexto = `Segunda-feira`
+//             return diaEmTexto;
+//         case 2:
+//             diaEmTexto = `Terça-feira`
+//             return diaEmTexto;
+//         case 3:
+//             diaEmTexto = `Quarta-feira`
+//             return diaEmTexto;
+//         case 4: 
+//         diaEmTexto = `Quinta-feira`
+//             return diaEmTexto;
+//         case 5:
+//             diaEmTexto = `Sexta-feira`
+//             return diaEmTexto;
+//         case 6:
+//             diaEmTexto = `Sabado`
+//             return diaEmTexto;
+//         default:
+//             diaEmTexto = ``
+//             return diaEmTexto;
+//     }
     
+// }
+
+
+//     function getMesEmTexto(texto) {
+//         switch(novaData[1]){
+//             case 0:
+//                 mesEmtexto = `Janeiro`
+//                 return mesEmtexto;
+//             case 1:
+//                 mesEmtexto = `Fevereiro`
+//                 return mesEmtexto;
+//             case 2:
+//                 mesEmtexto = `Março`
+//                 return mesEmtexto;
+//             case 3:
+//                 mesEmtexto = `Abril`
+//                 return mesEmtexto;
+//             case 4:
+//                 mesEmtexto = `Maio`
+//                 return mesEmtexto;
+//             case 5:
+//                 diaEmTexto = `Junho`
+//                 return diaEmTexto;
+//             case 6: 
+//                 mesEmtexto = `Julho`
+//                 return diaEmTexto;
+//             case 7:
+//                 mesEmtexto = `Agosto`
+//                 return mesEmtexto;
+//             case 8:
+//                 mesEmtexto = `Setembro`
+//                 return mesEmtexto;
+//             case 9:
+//                 mesEmtexto = `Outubro`
+//                 return mesEmtexto;
+//             case 10:
+//                 mesEmtexto = `Novembro`
+//                 return mesEmtexto;
+//             case 11:
+//                 mesEmtexto = `Dezembro`
+//                 return mesEmtexto;
+//                 default:
+//                     mesEmtexto = ``
+//                     return mesEmtexto;
+//         }  
+// }
+
+
+// diaEmTexto = getDiaEmTexto(novaData);
+// mesEmtexto = getMesEmTexto(novaData)
+//     console.log(`${diaEmTexto}, ${novaData[0]} de ${mesEmtexto} de ${novaData[2]} as ${novaData[3]}:${novaData[4]}`);
+
+
+//     const area = document.querySelector(`h2`)
+//     area.innerHTML = `Teu boga foi queimado hoje, ${diaEmTexto}, dia ${novaData[0]} de ${mesEmtexto} de ${novaData[2]} as ${novaData[3]}:${novaData[4]}`
+// }
+//     global();
+const h2 = document.querySelector(`h2`)
+const date = new Date();
+const opcoes = {
+    dateStyle: `full`,
+    timeSyle: `short`
 }
-
-
-    function getMesEmTexto(texto) {
-        switch(novaData[1]){
-            case 0:
-                mesEmtexto = `Janeiro`
-                return mesEmtexto;
-            case 1:
-                mesEmtexto = `Fevereiro`
-                return mesEmtexto;
-            case 2:
-                mesEmtexto = `Março`
-                return mesEmtexto;
-            case 3:
-                mesEmtexto = `Abril`
-                return mesEmtexto;
-            case 4:
-                mesEmtexto = `Maio`
-                return mesEmtexto;
-            case 5:
-                diaEmTexto = `Junho`
-                return diaEmTexto;
-            case 6: 
-                mesEmtexto = `Julho`
-                return diaEmTexto;
-            case 7:
-                mesEmtexto = `Agosto`
-                return mesEmtexto;
-            case 8:
-                mesEmtexto = `Setembro`
-                return mesEmtexto;
-            case 9:
-                mesEmtexto = `Outubro`
-                return mesEmtexto;
-            case 10:
-                mesEmtexto = `Novembro`
-                return mesEmtexto;
-            case 11:
-                mesEmtexto = `Dezembro`
-                return mesEmtexto;
-                default:
-                    mesEmtexto = ``
-                    return mesEmtexto;
-        }  
-}
-
-
-diaEmTexto = getDiaEmTexto(novaData);
-mesEmtexto = getMesEmTexto(novaData)
-    console.log(`${diaEmTexto}, ${novaData[0]} de ${mesEmtexto} de ${novaData[2]} as ${novaData[3]}:${novaData[4]}`);
-
-
-    const area = document.querySelector(`h2`)
-    area.innerHTML = `Teu boga foi queimado hoje, ${diaEmTexto}, dia ${novaData[0]} de ${mesEmtexto} de ${novaData[2]} as ${novaData[3]}:${novaData[4]}`
-}
-    global();
+h2.innerHTML = date.toLocaleString(`pt-BR`, opcoes)
