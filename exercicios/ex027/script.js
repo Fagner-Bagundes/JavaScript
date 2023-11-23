fd0e6c82770022f2a250ad04e286fd8c547f6744
Fagner_0 <ferreirabagundes@gmail.com>
2023-11-23 17:27:00 -0300

    aprendendo sobre a estrutura de repetição For

---
+++ exercicios/ex027/script.js
@@ -0,0 +1,13 @@
+const frutas = [`açai`, `banana`, `maça`, `melão`]
+
+
+for(let i = 0; i < frutas.length; i++ ){
+    console.log(`Fruta ${i}:`, frutas[i] );
+};
+
+
+
+// for (let i = 0; i <= 10; i++){
+//     const imparOuPar = i % 2 ===0 ? `par` : `impar`;
+//     console.log(`O numero ${i} é `, imparOuPar); 
+// }
