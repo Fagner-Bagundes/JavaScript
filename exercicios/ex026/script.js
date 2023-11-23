005c1970efd75d8f252b883be134b163c712b9ec
Fagner_0 <ferreirabagundes@gmail.com>
2023-11-23 16:19:25 -0300

    atribuição via desestruturação usando objetos

---
+++ exercicios/ex026/script.js
@@ -0,0 +1,12 @@
+const pessoa = {
+    nome: `Fagner`,
+    sobreNome: `Ferreira`,
+    idade: 18,
+    endereco: {
+        rua: `Dr. Braulino`,
+        numero: 764
+        } 
+};
+
+const { nome, sobreNome } = pessoa;
+console.log(nome, sobreNome);
