53778877c022b68bc56557ce07754d03bda55bfc
Fagner_0 <ferreirabagundes@gmail.com>
2023-11-25 17:10:02 -0300

    aprendendo  mais sobre DOM

---
+++ treino/script.js
@@ -0,0 +1,7 @@
+const conteiner = document.querySelector(`.conteiner`);
+
+let div = document.createElement(`div`);
+let texto = document.createTextNode(`Salve salve família`);
+
+div.appendChild(texto)
+conteiner.appendChild(div);
--- treino/tr2.js
+++
@@ -1,6 +0,0 @@
-const altura = 1.65;
-const peso = 56;
-
-const imc = peso / (altura * altura);
-
-console.log(imc);
