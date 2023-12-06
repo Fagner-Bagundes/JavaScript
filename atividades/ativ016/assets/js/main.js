f3994b7dec5b8b4dce5b79022834dad37ceb0146
Fagner_0 <ferreirabagundes@gmail.com>
2023-12-06 20:33:56 -0300

    lista de tarefas quase comcluida

--- atividades/ativ016/assets/js/main.js
+++ atividades/ativ016/assets/js/main.js
@@ -1,68 +1,61 @@
-const inputTarefa = document.querySelector(`.input-tarefa`)
-const btnTarefa = document.querySelector(`.btn-tarefa`)
-const tarefas = document.querySelector(`.tarefas`)
+ const inputTarefa = document.querySelector(`.input-tarefa`);
+ const btnTarefa = document.querySelector(`.btn-tarefa`);
+ const tarefas = document.querySelector(`.tarefas`);
 
-function criaLi() {
+ const apagar = tarefas.querySelector(`.apagar`);
+
+ function criaLi(){
     const li = document.createElement(`li`);
     return li;
-}
+ }
 
-inputTarefa.addEventListener(`keypress`, function(e){
-    if (e.keyCode === 13) {
+
+inputTarefa.addEventListener(`keypress`, function (e){
+    if (e.keyCode === 13){
         if (!inputTarefa.value) return;
         criaTarefa(inputTarefa.value);
-        
-    }
-
-});
-
-function limpaInput(){
-    inputTarefa.value =``;
-    inputTarefa.focus();
-}
-
-function criaBotaoApagar(li) {
-    li.innerText += ` `;
-    const BotaoApagar = document.createElement(`button`)
-    BotaoApagar.innerText = `apagar`
-    BotaoApagar.setAttribute(`class`, `apagar`)
-    li.appendChild(BotaoApagar)
-}
-
-
-function criaTarefa(textoInput) {
-  const li = criaLi();
-  li.innerText = textoInput;
-  tarefas.appendChild(li);
-  limpaInput();
-  criaBotaoApagar(li);
-  salvarTarefas();
-}
-
-btnTarefa.addEventListener(`click`, function() {
-    if (!inputTarefa.value) return;
-    criaTarefa(inputTarefa.value);
-  
-});
-
-document.addEventListener(`click`, function(e){
-    const el = e.target;
-    if (el.classList.contains(`apagar`)) {
-        el.parentElement.remove();
     }
 })
 
-function salvarTarefas() {
-    const liTarefas = tarefas.querySelectorAll(`li`)
-    const listaDeTarefas = [];
-
-        for (let tarefa of liTarefas){
-            let tarefaTexto = tarefa.innerText;
-            tarefaTexto = tarefaTexto.replace(`apagar`, ` `).trim();;
-            listaDeTarefas.push(listaDeTarefas)
-           
-        }
-
-        console.log(listaDeTarefas);
+function limpaImput(){
+    inputTarefa.value = ``;
+    inputTarefa.focus();
 }
+
+
+
+ function criaTarefa(textoInput) {
+    const li = criaLi();  
+    li.innerText = textoInput;
+    tarefas.appendChild(li);
+    limpaImput();
+    criaBotaoApagar(li);
+ }
+
+
+
+ function criaBotaoApagar(li){
+    li.innerText += ` `;
+    const botaoApagar = document.createElement(`button`);
+    botaoApagar.innerText = `Apagar`;
+    botaoApagar.setAttribute(`class`, `apagar`)
+    li.appendChild(botaoApagar)
+ }
+
+
+ btnTarefa.addEventListener(`click`, function(){
+    if (!inputTarefa.value) return;
+    criaTarefa(inputTarefa.value);
+ });
+
+ document.addEventListener(`click`, function(e){
+    const el = e.target;
+    if ( el.classList.contains(`apagar`)){
+       
+        el.parentElement.remove();
+    }
+
+ })
+
+
 
