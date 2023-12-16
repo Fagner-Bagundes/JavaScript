d3fc35cfccd5fc64a47e0599941b95afc37b2d6f
Fagner_0 <ferreirabagundes@gmail.com>
2023-12-16 19:36:30 -0300

    .

---
+++ atividades/ativ017/assets/js/main.js
@@ -0,0 +1,73 @@
+function criaCalculadora() {
+    return {
+        display: document.querySelector(`.display`),
+        btnClear: document.querySelector(`.btn-clear`),
+
+        inicia() {
+            this.clickBotoes();
+            this.pressionaEnter();
+            
+        },
+
+        pressionaEnter() {
+            this.display.addEventListener(`keyup`, (e) =>{
+                console.log(e);
+            })
+        },
+
+        realizaConta(){
+            let conta = this.display.value;
+            try {
+                conta = eval(conta)
+                if(!conta) {
+                    alert(`conta incalidade`)
+                    return;
+                }
+                this.display.value = conta;
+            } catch(e){
+                alert(`Conta inválida`);
+                return;
+            }
+        },
+
+        clearDisplay() {
+            this.display.value = ` `;
+        },
+
+        apagaUm() {
+            this.display.value = this.display.value.slice(0, -1)
+        },
+
+
+
+
+        clickBotoes() {
+             document.addEventListener(`click`, (e)=>{
+                const el = e.target;
+
+                if(el.classList.contains(`btn-num`)){
+                this.btnParadisplay(el.value); 
+                } 
+                
+                if(el.classList.contains(`btn-clear`)){
+                    this.clearDisplay();
+                }
+
+                if( el.classList.contains(`btn-del`)){
+                    this.apagaUm();
+                }
+
+                if(el.classList.contains(`btn-eq`)){
+                    this.realizaConta();
+                }
+             });
+        },
+
+        btnParadisplay(valor){
+          this.display.value += valor;
+        }
+    };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
