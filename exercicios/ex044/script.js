9ee075aa1eea63234a42046b528f08e94e3b9ad8
Fagner_0 <ferreirabagundes@gmail.com>
2023-12-19 17:27:53 -0300

    começando a estudar Array avançado

---
+++ exercicios/ex044/script.js
@@ -0,0 +1,13 @@
+//  valor por referência
+
+//                 0         1        2          3         4
+// const nomes = [ `Eduardo`, `Maria`, `Joana`, `Fagner`, `Eduardo`];
+// const slice = nomes.slice(0, -3)
+// console.log(slice);
+// console.log(nomes);
+
+const nome = `Luiz Otávio Miranda`;
+const nomes =  nome.split(` `)
+console.log(nomes);
+
+
