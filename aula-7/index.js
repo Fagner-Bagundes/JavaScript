2349a1fab776d9aff4b5407f2b2d10c72f394de1
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-25 17:48:58 -0300

    entendendo consts

---
+++ aula-7/index.js
@@ -0,0 +1,12 @@
+const n1 = 10
+const n2 = 10
+const resultado = n1 + n2
+const multiplicaResultado = resultado * 2
+let triplicaResultado = resultado * 3
+triplicaResultado = triplicaResultado + 5
+
+console.log(resultado)
+console.log(multiplicaResultado)
+console.log(triplicaResultado)
+
+console.log(`o  tipo de dado do resultado é`, typeof(resultado))
