76519c3bbe866379296cd54a66172a0488560331
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-25 17:58:55 -0300

    exercício com variáveis e constantes

---
+++ aula-7/ex-1/index.js
@@ -0,0 +1,8 @@
+const nome = `Fagner`;
+const sobreNome = `Ferreira Bgundes`;
+const idade = 19;
+let peso = 59;
+const alturaEmM = 1.65
+let iMC =  peso/ (alturaEmM  * alturaEmM)
+
+console.log(`${nome} ${sobreNome} tem ${idade} anos, pesa ${peso} kg, tem ${alturaEmM} e seu IMC é de ${iMC}`)
