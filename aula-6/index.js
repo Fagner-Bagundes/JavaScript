7113973c8327f255f9307d0f5efa3433bd78262a
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-25 20:57:49 -0300

    entendo  operadores aritméticos  e concatenações

---
+++ aula-6/index.js
@@ -0,0 +1,60 @@
+/**
+ * Aritiméticos
+ * + Adição / concatenação
+ * - subtração
+ *  / divisão 
+ *     * multipricação
+ *     ** portencição
+ * % resto da divisão
+ * 
+ * 
+ */
+
+/**
+ * Precedência
+ * ()
+ * **
+ * * / %
+ * + -
+ */
+
+/**
+ * incremento = ++
+ * decremento = --
+ */
+
+// let num1 = 10
+// let num2 = 3
+// let concat = num1 + num2 + (9 - 7) * 6
+// console.log(concat, typeof(concat));
+
+// let contador = 1;
+// ++contador;
+// --contador;
+// ++contador;
+// ++contador;
+// console.log(contador)
+
+// const passo = 2
+// let contador2 = 0;
+// contador2 += passo
+// contador2 += passo
+// console.log(contador2);
+
+// const passo3 = 2
+// let contador3 = 2;
+// contador3 *= passo3
+// contador3 *= passo3
+// contador3 *= passo3
+// contador3 *= passo3
+// contador3 *= passo3
+
+// console.log(contador3);
+
+
+let numero1 = 10
+let numero2 = Number(`20.5`)
+
+console.log(numero1 + numero2);
+
+
