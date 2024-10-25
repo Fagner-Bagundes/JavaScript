3c1e3eb944cba712195189a39a9a501bf374a896
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-25 20:14:50 -0300

    aprendendo sobre dados primitivos

---
+++ aula-5/index.js
@@ -0,0 +1,17 @@
+// Tipos de dados primitivos
+const nome = `Fagner`; // string
+const num1 = 10; // number
+const num2 = 10.5; // number
+let nomeAluno; // hundefined é um valor indefinido
+const sobrenomeAluno = null; // null é um valor nulo
+const aprovado = false; // boolean
+const reprovado = true; // boolean
+
+console.log(typeof(nome));
+console.log(typeof(num1));
+console.log(typeof(num2));
+console.log(typeof(nomeAluno));
+console.log(typeof(sobrenomeAluno));
+console.log(typeof(aprovado));
+console.log(typeof(reprovado));
+
