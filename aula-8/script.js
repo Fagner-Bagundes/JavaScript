e22f62b3fb31d1e43985548f1d6073cb134842a8
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-26 14:30:29 -0300

    Estudo sobre métodos e propriedades de strings finalizado

--- aula-8/script.js
+++ aula-8/script.js
@@ -1,5 +1,5 @@
-        //  0123456789......
-let nome = `Fagner ferreira`
+        //  0123456789.....
+let nome = `Fagner Ferreira`
 
 // console.log(`TRABALHANDO COM A STRING DA VARIÁVEL NOME, QUE TEM O VALOR DE ${nome}`);
 // console.log(` `);
@@ -36,7 +36,57 @@ console.log(nome.search(`ferreira`));
 
 
 console.log(`-----------------------------`);
-console.log(`substituindo um único valor da string usando o metodo replace() sem expressões regulares `);
+console.log(`substituindo um o valor "Ferreira" da string por "Bagundes" usando o metodo replace() sem expressões regulares `);
+console.log (nome.replace(`Ferreira`, `Bagundes`));
+console.log(` `);
+console.log(`Substituindo todos os valores "r" da string por "B", usando o metodo replace(), com expressões regulares`);
+console.log(nome.replace(/r/g, `B`));
+
+console.log(`-----------------------------`);
+console.log(`Descobrindo o tamanho da variável nome com o .langth`);
+console.log(nome.length);
+
+console.log(`-----------------------------`);
+let text = `era uma vez em um passado muito distante`
+console.log(`fatiando a string da parte "era uma vez" da variavel text = ${text}". usando o método slice()`);
+console.log(``);
+
+console.log(`-----------------------------`);
+console.log(`devidindo elementos de uma string em vários valores de uma array, usando o método: split(). passondo como sepadado o " "`);
+console.log(text.split(` `));
+console.log(` `);
+console.log(`Fazendo com que o split pegue apenas 3 elementos`);
+console.log(text.split(" ", 3));
+
+console.log(`-----------------------------`);
+console.log(`Transformando todas as caracteres da string em maiúsculo`);
+console.log(text.toUpperCase());
+console.log(` `);
+console.log(`Transformando todas as caracteres da string em minúsculo`);
+console.log(text.toLowerCase());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
