5278268785ab2d2a0ff34a25c02a7a87221e3ecf
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-25 22:53:52 -0300

    exercitando os assuntos da aula 7

---
+++ aula-7/js/ativ.js
@@ -0,0 +1,5 @@
+let num1 = Number(prompt(`Digite o primeiro numero`))
+let num2 = Number(prompt(`Digite o segundo numero`))
+
+console.log(`o resultado da soma entre ${num1} e ${num2} =`, num1 + num2);
+
--- aula-7/js/script.js
+++ aula-7/js/script.js
@@ -3,14 +3,14 @@
 // const comfirme = confirm(`aperte cancelar ou ok`);
 // console.log(comfirme)
 
-const resposta = prompt(`digite sua idade`);
-let numero = parseFloat(resposta);
-let numero2 =parseInt(resposta);
+// const resposta = prompt(`digite sua idade`);
+// let numero = parseFloat(resposta);
+// let numero2 =parseInt(resposta);
 
 
-console.log(resposta, typeof(resposta));
-console.log(numero, typeof(numero));
-console.log(numero2, typeof(numero2));
+// console.log(resposta, typeof(resposta));
+// console.log(numero, typeof(numero));
+// console.log(numero2, typeof(numero2));
 
 
 
