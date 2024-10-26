733acb6f8a311557af7bea57d4ab47b04c2b8146
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-25 22:39:47 -0300

    entendo alert, prompt, comfirm

---
+++ aula-7/js/script.js
@@ -0,0 +1,13 @@
+// const alerta = alert(`hellooo`);
+
+// const comfirme = confirm(`aperte cancelar ou ok`);
+// console.log(comfirme)
+
+const resposta = prompt(`digite sua idade`)
+let numero = parseFloat(resposta)
+console.log(resposta, typeof(resposta))
+console.log(numero, typeof(numero));
+console.log(15, typeof(15));
+
+
+
