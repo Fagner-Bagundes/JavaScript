ca06dbc36323c827f71361599f5228bcd7937610
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-25 22:48:45 -0300

    aprendendo a converter um dado de tipo string em um dado Number

--- aula-7/js/script.js
+++ aula-7/js/script.js
@@ -3,11 +3,15 @@
 // const comfirme = confirm(`aperte cancelar ou ok`);
 // console.log(comfirme)
 
-const resposta = prompt(`digite sua idade`)
-let numero = parseFloat(resposta)
-console.log(resposta, typeof(resposta))
+const resposta = prompt(`digite sua idade`);
+let numero = parseFloat(resposta);
+let numero2 =parseInt(resposta);
+
+
+console.log(resposta, typeof(resposta));
 console.log(numero, typeof(numero));
-console.log(15, typeof(15));
+console.log(numero2, typeof(numero2));
+
 
 
 
