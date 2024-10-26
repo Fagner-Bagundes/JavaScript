e61fea9c7b1ad2cc41a971049d4b116c23edd2bc
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-26 17:42:06 -0300

    atividade de manipular strings feita

--- aula-8/ativ/script.js
+++ aula-8/ativ/script.js
@@ -1,8 +1,23 @@
-const nome = prompt(`Digite o seu nome`)
-console.log(nome);
+const nome = prompt(`Digite o seu nome`);
+const titulo = document.querySelector(`.titulo`);
 
-let titulo = document.querySelector(`.titulo`)
 
-console.log(titulo);
+let tantoLetras = nome.replace(/ /g, "");
 
-titulo.innerHTML += nome
+
+titulo.innerHTML += `Seu nome é: ${nome} <br>`
+titulo.innerHTML += `Seu nome tem ${tantoLetras.length} letras <br>`
+titulo.innerHTML += `A segunda letra do seu nome é: ${nome[1]} <br>`
+titulo.innerHTML += `o primeiro índice da letra "r" no seu nome é: ${nome.indexOf(`r`)} <br>`
+titulo.innerHTML += `o último índice da letra "r" do seu nome é ${nome.lastIndexOf(`r`)} <br>`
+titulo.innerHTML += `As 3 últimas letras do seu nome são: ${ nome.slice(-3)} <br>`
+titulo.innerHTML += `As palavras do seu nome são: ${nome.split(/ /g)} <br>`
+titulo.innerHTML += `Seu nome com letras maiúsculas: ${nome.toUpperCase()} <br>`
+titulo.innerHTML += `Seu nome com letras minúsculas: ${nome.toLowerCase()} <br>`
+
+
+
+
+
+
+
--- aula-8/script.js
+++ aula-8/script.js
@@ -1,103 +1,66 @@
-        //  0123456789.....
-let nome = `Fagner Ferreira`
+//         //  0123456789.....
+// let nome = `Fagner Ferreira`
 
-// console.log(`TRABALHANDO COM A STRING DA VARIÁVEL NOME, QUE TEM O VALOR DE ${nome}`);
+// // console.log(`TRABALHANDO COM A STRING DA VARIÁVEL NOME, QUE TEM O VALOR DE ${nome}`);
+// // console.log(` `);
+
+// // console.log(`Descobrindo o valor do índice 5 da variável`);
+// // console.log(nome[5]);
+// // console.log(`_________________________________`);
+// // console.log(`Descobrindo o valor do índice 5 com o charAt()`);
+// // console.log(nome.charAt(5));
+
+// // console.log(`_________________________________`); 
+// // console.log(`Concatenando a variável com o concat()`);
+// // console.log(nome.concat(` Bagundes`));
+
+// // console.log(`_________________________________`); 
+// // console.log(`Descobrindo o índice de um valor "ferreira" com o indexOf()`);
+// // console.log(nome.indexOf(`ferreira`));
+// // console.log(`_________________________________`); 
+// // console.log(`Descobrindo o índice do último caractere "e" com o lastIndexOf()`);
+// // console.log(nome.lastIndexOf(`e`,13));
+// // console.log(`_________________________________`); 
+// // console.log(` `);
+
+// console.log(`EXPRESSÕES REGULARES`);
 // console.log(` `);
+// console.log(`pegando todas as letras "r" usando a função match`);
+// console.log(nome.match(/r/g ));
+// console.log(`pegando todas as letras minúsculas com a função match`);
+// console.log(nome.match(/[a-z]/g));
 
-// console.log(`Descobrindo o valor do índice 5 da variável`);
-// console.log(nome[5]);
-// console.log(`_________________________________`);
-// console.log(`Descobrindo o valor do índice 5 com o charAt()`);
-// console.log(nome.charAt(5));
+// console.log(`-----------------------------`);
+// console.log(`achando o índice do valor ferreira dentro da string com o metodo search()`);
+// console.log(nome.search(`ferreira`));
 
-// console.log(`_________________________________`); 
-// console.log(`Concatenando a variável com o concat()`);
-// console.log(nome.concat(` Bagundes`));
 
-// console.log(`_________________________________`); 
-// console.log(`Descobrindo o índice de um valor "ferreira" com o indexOf()`);
-// console.log(nome.indexOf(`ferreira`));
-// console.log(`_________________________________`); 
-// console.log(`Descobrindo o índice do último caractere "e" com o lastIndexOf()`);
-// console.log(nome.lastIndexOf(`e`,13));
-// console.log(`_________________________________`); 
+// console.log(`-----------------------------`);
+// console.log(`substituindo um o valor "Ferreira" da string por "Bagundes" usando o metodo replace() sem expressões regulares `);
+// console.log (nome.replace(`Ferreira`, `Bagundes`));
 // console.log(` `);
+// console.log(`Substituindo todos os valores "r" da string por "B", usando o metodo replace(), com expressões regulares`);
+// console.log(nome.replace(/r/g, `B`));
 
-console.log(`EXPRESSÕES REGULARES`);
-console.log(` `);
-console.log(`pegando todas as letras "r" usando a função match`);
-console.log(nome.match(/r/g ));
-console.log(`pegando todas as letras minúsculas com a função match`);
-console.log(nome.match(/[a-z]/g));
+// console.log(`-----------------------------`);
+// console.log(`Descobrindo o tamanho da variável nome com o .langth`);
+// console.log(nome.length);
 
-console.log(`-----------------------------`);
-console.log(`achando o índice do valor ferreira dentro da string com o metodo search()`);
-console.log(nome.search(`ferreira`));
+// console.log(`-----------------------------`);
+// let text = `era uma vez em um passado muito distante`
+// console.log(`fatiando a string da parte "era uma vez" da variavel text = ${text}". usando o método slice()`);
+// console.log(``);
 
+// console.log(`-----------------------------`);
+// console.log(`devidindo elementos de uma string em vários valores de uma array, usando o método: split(). passondo como sepadado o " "`);
+// console.log(text.split(` `));
+// console.log(` `);
+// console.log(`Fazendo com que o split pegue apenas 3 elementos`);
+// console.log(text.split(" ", 3));
 
-console.log(`-----------------------------`);
-console.log(`substituindo um o valor "Ferreira" da string por "Bagundes" usando o metodo replace() sem expressões regulares `);
-console.log (nome.replace(`Ferreira`, `Bagundes`));
-console.log(` `);
-console.log(`Substituindo todos os valores "r" da string por "B", usando o metodo replace(), com expressões regulares`);
-console.log(nome.replace(/r/g, `B`));
-
-console.log(`-----------------------------`);
-console.log(`Descobrindo o tamanho da variável nome com o .langth`);
-console.log(nome.length);
-
-console.log(`-----------------------------`);
-let text = `era uma vez em um passado muito distante`
-console.log(`fatiando a string da parte "era uma vez" da variavel text = ${text}". usando o método slice()`);
-console.log(``);
-
-console.log(`-----------------------------`);
-console.log(`devidindo elementos de uma string em vários valores de uma array, usando o método: split(). passondo como sepadado o " "`);
-console.log(text.split(` `));
-console.log(` `);
-console.log(`Fazendo com que o split pegue apenas 3 elementos`);
-console.log(text.split(" ", 3));
-
-console.log(`-----------------------------`);
-console.log(`Transformando todas as caracteres da string em maiúsculo`);
-console.log(text.toUpperCase());
-console.log(` `);
-console.log(`Transformando todas as caracteres da string em minúsculo`);
-console.log(text.toLowerCase());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// console.log(`-----------------------------`);
+// console.log(`Transformando todas as caracteres da string em maiúsculo`);
+// console.log(text.toUpperCase());
+// console.log(` `);
+// console.log(`Transformando todas as caracteres da string em minúsculo`);
+// console.log(text.toLowerCase());
