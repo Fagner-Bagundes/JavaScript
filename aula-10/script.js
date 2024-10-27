6ede2e93ee9dff6fa82cafaaeac22d88178ebefc
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-27 11:05:26 -0300

    entendendo o objeto Math

---
+++ aula-10/script.js
@@ -0,0 +1,16 @@
+num1 = 9
+
+console.log(num1 ** (0.5));
+
+
+num2 = Math.floor(num1)
+let num2 = Math.ceil(num1)
+let num2 = Math.round(num1)
+console.log(Math.max(15, 20, 23, 35,88.6, 88.5));
+console.log(Math.min(15,20.3,3,9,19,2.9, 2.4, 0.7))
+console.log(Math.round((Math.random() * 20)));
+
+
+
+
+
