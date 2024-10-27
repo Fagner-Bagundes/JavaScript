3c4a625ffc380a3ea7374e7351ac167e27188474
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-27 12:25:18 -0300

    praticando cada metodo do Math

--- aula-10/script.js
+++ aula-10/script.js
@@ -1,14 +1,40 @@
-num1 = 9
+function barra() {
+    console.log(`______________________________________`);
+}
+function espaco() {
+    console.log(` `);
+    
+    
+}
 
-console.log(num1 ** (0.5));
+let num1 = 15.6
+console.log(`Arredondando para baixo o valor ${num1} da variável num1`);
+console.log(Math.floor(num1))
+barra()
+console.log(`Arredondando para cima o valor ${num1} da variavel num1`);
+console.log(Math.ceil(num1));
+barra()
+console.log(`Arredondando o valor ${num1} para o valor mais próximo`);
+console.log(Math.round(num1))
+barra()
+console.log(`Descobrindo o menor valor de`, 10, 20., 5.5, 9004)
+console.log(`o menor numero é: `,Math.min(10, 20, 5.5, 9004))
+barra()
+console.log(`Descobrindop o menor valor de`, 10, 20., 5.5, 9004)
+console.log(`o maior numero é: `,Math.max(10, 20, 5.5, 9004))
+barra()
+console.log(`Gerando  um numero aleatórios com o metodo random():`)
+console.log(Math.random());
+espaco()
+console.log(`Gerando um numero aleatório e inteiro de 0 a 10:`)
+console.log(Math.random() * (10))
+
+espaco()
+
+console.log(`Gerando um numero aleatório de 10 a 20`);
+console.log(Math.random() * (10) + 10)
 
 
-num2 = Math.floor(num1)
-let num2 = Math.ceil(num1)
-let num2 = Math.round(num1)
-console.log(Math.max(15, 20, 23, 35,88.6, 88.5));
-console.log(Math.min(15,20.3,3,9,19,2.9, 2.4, 0.7))
-console.log(Math.round((Math.random() * 20)));
 
 
 
