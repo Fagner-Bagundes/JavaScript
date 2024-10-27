a265f1f8a2b73cbf0fe266331bf07d75e5e29a2d
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-26 22:22:53 -0300

    entendendo o toString

---
+++ aula-9/script.js
@@ -0,0 +1,20 @@
+function colocaBarra() {
+    console.log("__________________________");
+    return
+}
+let num1 = 20
+console.log(`APRENDENDO A MANIPULAR NUMBERS`);
+console.log(` `);
+console.log(`Transformando um number: ${num1} da variável num1 em string com toString()`);
+console.log( num1.toString(),`=`, typeof(num1.toString()));
+colocaBarra();
+console.log(`colocanco mostrando o numero de variavel em binário`)
+console.log(num1.toString(2));
+
+
+
+
+
+
+
+
