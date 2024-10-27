95ed96142aa14349ff9797b055a8b2ca25f9757a
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-27 19:34:18 -0300

    fazendo cálculos com funções

--- aula-12/script.js
+++ aula-12/script.js
@@ -1,17 +1,26 @@
-function saudacao() {
-    console.log(`olá mundo`)
-};
+// function saudacao() {
+//     console.log(`olá mundo`)
+// };
 
-saudacao()
+// saudacao()
 
-function saudacao2(){
-    return `Olá meu Mundo`
+// function saudacao2(){
+//     return `Olá meu Mundo`
+// }
+
+// let saudacaoV = saudacao2()
+// console.log(saudacaoV)
+
+// function saudacao3(nome){
+//     console.log( `Olá meu amigo ${nome}`)
+// }
+// saudacao3(`Fagner`)
+
+
+function soma(x, y){
+    const resultado = ((x * 100) + (y * 100)) / 100
+    return resultado
 }
+const resultado = soma(1, 1)
 
-let saudacaoV = saudacao2()
-console.log(saudacaoV)
-
-function saudacao3(nome){
-    console.log( `Olá meu amigo ${nome}`)
-}
-saudacao3(`Fagner`)
+console.log(resultado)
