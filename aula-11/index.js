75a84344181ee4db611d5d52ba82158d1d69d3a5
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-27 17:37:05 -0300

    aprendendo a alterar indices

--- aula-11/index.js
+++ aula-11/index.js
@@ -1,8 +1,8 @@
 // Criando um Array:
 let nomes = [`Fagner`,`Nalda`,`Francisco`]
 console.log(nomes)
-// // alterando um indice da Array:
-// nomes[0] = `Francico`
+// alterando um indice da Array:
+nomes[0] = `Francico`
 // console.log(nomes);
 // // Adicionando um  novo indice:
 // nomes[3] = `Fagner`
