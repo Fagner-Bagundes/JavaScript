5386c3d208229255ceb1b4f0b07a241507b8ad76
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-28 15:34:04 -0300

    entendendo os operadores de comparação

---
+++ Estudos/Lógica-de-Programação/ex-1/scipts.js
@@ -0,0 +1,21 @@
+/**
+ * >   -  Maior que
+ * >=  - Maior ou igual a
+ * <   - Menor que
+ * <=  - Menor ou igual a
+ * ==  - igual a(valores)
+ * === - igual a(valores e tipo)
+ * !=  - diferente de(valores)
+ * !== - diferente de(valores e tipo)
+ */
+
+const comp = 10 > 5;
+console.log(`10 é maior do que 5?: `, comp);
+const comp2 = 20 >= 20;
+console.log(`20 é maior ou igual a 20:?`, comp2);
+const comp3 = 25 <= 26;
+console.log(`25 é menor ou igual a 26?:`, comp3)
+const comp4 = 30 === 20;
+console.log(`30 é igual a 25?: `,comp4)
+const comp5 = 100 !== 200;
+console.log(`100 é dferente de 200?: `, comp5)
