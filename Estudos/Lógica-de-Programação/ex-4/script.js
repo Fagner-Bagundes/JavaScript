f874601e4f99e4af25417dd1caaf5c25fb57973e
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-28 20:32:23 -0300

    fazendo o HTML para um desafio

---
+++ Estudos/Lógica-de-Programação/ex-4/script.js
@@ -0,0 +1,16 @@
+// const hora = 6;
+
+// if (hora>=6 && hora<12){
+//     console.log(`diga bom dia`)
+// }else if(hora>12 && hora<19){
+//     console.log(` Boa tarde`);
+// }else{
+//     console.log(`Boa noite`)
+// };
+
+
+const numero = 5;
+
+if (nume) {
+    
+}
