46805bfafa4ca0c7485fdae3940346d2f8b8e2b4
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-28 17:28:38 -0300

    praticando estrutura de condição

--- Estudos/Lógica-de-Programação/ex-3/script.js
+++ Estudos/Lógica-de-Programação/ex-3/script.js
@@ -10,3 +10,42 @@ if (pontuacao === 200) {
 }
 
 console.log(nivel)
+
+
+
+
+
+let notaAluno = {
+    matematica: 3,
+    portugues: 3,
+    fisica: 3,
+    artes: 3,
+}
+
+function mediaCalc(){
+    let soma = (notaAluno.matematica + notaAluno.portugues + notaAluno.fisica + notaAluno.artes) / 4;
+    return soma;
+    
+};
+
+let mediaAluno = mediaCalc();
+let estadoAluno;
+const mediaAprovado = 6;
+const mediaReprovado = 3;
+
+
+
+
+
+
+if (mediaAluno >= mediaAprovado){
+    estadoAluno = `Aluno Aprovado`
+} else if(mediaAluno > mediaReprovado && mediaAluno < mediaAprovado){
+    estadoAluno = `Aluno em recuperação`    
+}else{
+    estadoAluno = `Aluno Reprovado`
+}
+
+console.log(`Nome do Aluno: Fagner Ferreira`)
+console.log(`Media do Aluno: ${mediaAluno}`);
+console.log(`O aluno está: `,estadoAluno)
