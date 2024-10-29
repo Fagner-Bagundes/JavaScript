c3715b6a757fd7f4ae8869db8de23aaffbd918d5
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-29 15:19:49 -0300

    ajustando

--- desafios/2-Calculadora-de-IMC/assets/js/script.js
+++ desafios/2-Calculadora-de-IMC/assets/js/script.js
@@ -3,6 +3,7 @@
 const formulario = document.querySelector(`.formulario`)
 const btn = document.querySelector('.btn')
 const resultadoInput = document.querySelector(`.resultado-calculo`)
+let iniciarMostraResultado = true; 
 let pesoInput;
 let alturaInput;
 let pesoN;
@@ -12,22 +13,24 @@ let estadosR = [`Abaixo do peso`, `Peso Normal`, `Sobrepeso`, `Obesidade Grau 1`
 
 
 function mostraResultado(){
-    if (valorIMC > 40) {
-        resultadoInput.innerHTML =`<p> Seu IMC é ${valorIMC} e você está: ${estadosR[5]}</p>`;
-    }else if (valorIMC <= 39.9 && valorIMC>= 35){
-        resultadoInput.innerHTML =`<p> Seu IMC é ${valorIMC} e você está: ${estadosR[4]}</p>`;
-
-    } else if(valorIMC <= 34.9 && valorIMC>= 30){
-        resultadoInput.innerHTML =`<p> Seu IMC é ${valorIMC} e você está: ${estadosR[3]}</p>`;
-
-    } else if(valorIMC <= 29.9 && valorIMC >= 25){
-        resultadoInput.innerHTML =`<p> Seu IMC é ${valorIMC} e você está: ${estadosR[2]}</p>`;
-
-    } else if(valorIMC<= 24.9 && valorIMC >= 18.5){
-        resultadoInput.innerHTML =`<p> Seu IMC é ${valorIMC} e você está: ${estadosR[1]}</p>`;
-
-    } else {
-        resultadoInput.innerHTML =`<p> Seu IMC é ${valorIMC} e você está: ${estadosR[0]}</p>`;
+    if (iniciarMostraResultado === true) {
+        if (valorIMC > 40) {
+            resultadoInput.innerHTML =`<p> Seu IMC é ${valorIMC} e você está: ${estadosR[5]}</p>`;
+        }else if (valorIMC <= 39.9 && valorIMC>= 35){
+            resultadoInput.innerHTML =`<p> Seu IMC é ${valorIMC} e você está: ${estadosR[4]}</p>`;
+    
+        } else if(valorIMC <= 34.9 && valorIMC>= 30){
+            resultadoInput.innerHTML =`<p> Seu IMC é ${valorIMC} e você está: ${estadosR[3]}</p>`;
+    
+        } else if(valorIMC <= 29.9 && valorIMC >= 25){
+            resultadoInput.innerHTML =`<p> Seu IMC é ${valorIMC} e você está: ${estadosR[2]}</p>`;
+    
+        } else if(valorIMC<= 24.9 && valorIMC >= 18.5){
+            resultadoInput.innerHTML =`<p> Seu IMC é ${valorIMC} e você está: ${estadosR[1]}</p>`;
+    
+        } else {
+            resultadoInput.innerHTML =`<p> Seu IMC é ${valorIMC} e você está: ${estadosR[0]}</p>`;
+        }
     }
 }
 
@@ -38,8 +41,12 @@ function verificaDados() {
 
     if (pesoN === 0 && alturaN === 0) {
         resultadoInput.innerHTML =`Digite alguma coisa nas caixas de textos`
+        iniciarMostraResultado = false;
     } else if (isNaN(alturaN) && isNaN(pesoN)) {
         resultadoInput.innerHTML =`Digite Numeros validos nos campos de textos`
+        
+        resultadoInput.classList.add(`.resultado-invalido`)
+        iniciarMostraResultado = false;
     } else {
         alturaInvalida()
         pesoInvalido()
@@ -51,8 +58,10 @@ function verificaDados() {
 
         if (isNaN(pesoN)) {
             resultadoInput.innerHTML =`O valor de peso é inválido!!`;
+            iniciarMostraResultado = false;
         } else if (pesoN >= 350) {
             resultadoInput.innerHTML =`Digite um valor menor na area de peso`;
+            iniciarMostraResultado = false;
 
         }
     }
@@ -61,8 +70,10 @@ function verificaDados() {
 
         if (isNaN(alturaN)) {
             resultadoInput.innerHTML =`O valor da altura é inválido!!`;
+            iniciarMostraResultado = false;
         } else if (alturaN >= 3) {
             resultadoInput.innerHTML =`Digite um valor menor na area de altura`;
+            iniciarMostraResultado = false;
 
         }
     }
@@ -74,7 +85,6 @@ function calculaIMC() {
     let calc = pesoN / (alturaN * alturaN)
     valorIMC = calc.toFixed(1)
     console.log(valorIMC)
-    mostraResultado()
 }
 
 
@@ -84,13 +94,12 @@ function clickOnButton(event) {
     pesoInput = document.querySelector(`.peso-input`);
     alturaInput = document.querySelector('.altura-input');
     pesoN = Number(pesoInput.value)
-    alturaN = Number(alturaInput.value)
+    alturaN = Number(alturaInput.value);
 
-        ;
     verificaDados()
+    calculaIMC()
+    mostraResultado()
     
-
-
 }
 
 
