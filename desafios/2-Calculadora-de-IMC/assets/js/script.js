6da6f1eb67537b0e97784cbcec2e024e5aeae7e2
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-29 16:43:07 -0300

    Projeto finalizado com sucesso

--- desafios/2-Calculadora-de-IMC/assets/js/script.js
+++ desafios/2-Calculadora-de-IMC/assets/js/script.js
@@ -1,65 +1,37 @@
 
-
 const formulario = document.querySelector(`.formulario`)
 const btn = document.querySelector('.btn')
 const resultadoInput = document.querySelector(`.resultado-calculo`)
-let iniciarMostraResultado = true; 
+let iniciarMostraResultado; 
 let pesoInput;
 let alturaInput;
 let pesoN;
 let alturaN;
 let valorIMC;
-let estadosR = [`Abaixo do peso`, `Peso Normal`, `Sobrepeso`, `Obesidade Grau 1`, `Obedidade Grau 2`, `Obesidade Grau 3`]
+let estadosR = [`Abaixo do peso`, `com o peso Normal`, `Com sobrepeso`, ` com obesidade Grau 1`, `com obesidade Grau 2`, `com obesidade Grau 3`]
 
 
 formulario.addEventListener(`submit`, clickOnButton)
 
-function adicionaOuRemoveEstilos(){
-    if (iniciarMostraResultado === true) {
-        resultadoInput.classList.remove(`resultado-invalido`)
-        resultadoInput.classList.add(`resultado-valido`)
-    } else {
-        resultadoInput.classList.remove(`resultado-valido`)
-        resultadoInput.classList.add(`resultado-invalido`)
-    };
-}
-
-function mostraResultado(){
-    if (iniciarMostraResultado === true) {
-        if (valorIMC > 40) {
-            resultadoInput.innerHTML =`<p> Seu IMC é ${valorIMC} e você está: ${estadosR[5]}</p>`;
-        }else if (valorIMC <= 39.9 && valorIMC>= 35){
-            resultadoInput.innerHTML =`<p> Seu IMC é ${valorIMC} e você está: ${estadosR[4]}</p>`;
-    
-        } else if(valorIMC <= 34.9 && valorIMC>= 30){
-            resultadoInput.innerHTML =`<p> Seu IMC é ${valorIMC} e você está: ${estadosR[3]}</p>`;
-    
-        } else if(valorIMC <= 29.9 && valorIMC >= 25){
-            resultadoInput.innerHTML =`<p> Seu IMC é ${valorIMC} e você está: ${estadosR[2]}</p>`;
-    
-        } else if(valorIMC<= 24.9 && valorIMC >= 18.5){
-            resultadoInput.innerHTML =`<p> Seu IMC é ${valorIMC} e você está: ${estadosR[1]}</p>`;
-    
-        } else {
-            resultadoInput.innerHTML =`<p> Seu IMC é ${valorIMC} e você está: ${estadosR[0]}</p>`;
-        }
-    }
-}
-
 
 
 function verificaDados() {
-
+    
     if (pesoN === 0 && alturaN === 0) {
         resultadoInput.innerHTML =`Digite alguma coisa nas caixas de textos`
         iniciarMostraResultado = false;
         
+    } else if(pesoN === 0 || alturaN === 0){
+        resultadoInput.innerHTML =`
+        Faltou faltou digitar dados em um um dos camopos de textp`
     } else if (isNaN(alturaN) && isNaN(pesoN)) {
         resultadoInput.innerHTML =`Digite Numeros validos nos campos de textos`
-        
-        
         iniciarMostraResultado = false;
-    } else {
+    } else if(alturaN <=1 || pesoN<=10){
+        resultadoInput.innerHTML =`Digite Numeros validos nos campos de textos`
+    }
+    
+    else {
         iniciarMostraResultado = true;
         alturaInvalida()
         pesoInvalido()
@@ -68,24 +40,25 @@ function verificaDados() {
 
 
     function pesoInvalido() {
-
+        
         if (isNaN(pesoN)) {
-            resultadoInput.innerHTML =`O valor de peso é inválido!!`;
+            resultadoInput.innerHTML = `O valor de peso é inválido!!`;
             iniciarMostraResultado = false;
             
         } else if (pesoN >= 350) {
             resultadoInput.innerHTML =`Digite um valor menor na area de peso`;
             iniciarMostraResultado = false;
             
-        } else{
-            iniciarMostraResultado = true
         }
     }
-
+    
+    
+    
+    
     function alturaInvalida() {
-
+        
         if (isNaN(alturaN)) {
-            resultadoInput.innerHTML =`O valor da altura é inválido!!`;
+            resultadoInput.innerHTML = `O valor de altura é inválido!!`;
             iniciarMostraResultado = false;
             
         } else if (alturaN >= 3) {
@@ -97,15 +70,55 @@ function verificaDados() {
         }
     }
 
+    
+
+    
+
 
 }
 
 function calculaIMC() {
     let calc = pesoN / (alturaN * alturaN)
     valorIMC = calc.toFixed(1)
-    console.log(valorIMC)
 }
 
+function mostraResultado(){
+    calculaIMC()
+    
+    if (iniciarMostraResultado === true) {
+        if (valorIMC > 40) {
+            resultadoInput.innerHTML =`<p> Seu IMC é ${valorIMC} e você está: ${estadosR[5]}</p>`;
+            iniciarMostraResultado = false;
+        }else if (valorIMC <= 39.9 && valorIMC>= 35){
+            resultadoInput.innerHTML =`<p> Seu IMC é ${valorIMC} e você está: ${estadosR[4]}</p>`;
+            iniciarMostraResultado = false;
+            
+        } else if(valorIMC <= 34.9 && valorIMC>= 30){
+            resultadoInput.innerHTML =`<p> Seu IMC é ${valorIMC} e você está: ${estadosR[3]}</p>`;
+            iniciarMostraResultado = false;
+    
+        } else if(valorIMC <= 29.9 && valorIMC >= 25){
+            resultadoInput.innerHTML =`<p> Seu IMC é ${valorIMC} e você está: ${estadosR[2]}</p>`;
+            
+        } else if(valorIMC<= 24.9 && valorIMC >= 18.5){
+            resultadoInput.innerHTML =`<p> Seu IMC é ${valorIMC} e você está: ${estadosR[1]}</p>`;
+    
+        } else {
+            resultadoInput.innerHTML =`<p> Seu IMC é ${valorIMC} e você está: ${estadosR[0]}</p>`;
+            iniciarMostraResultado = false;
+        }
+    }
+}
+
+function adicionaOuRemoveEstilos(){
+    if (iniciarMostraResultado === true) {
+        resultadoInput.classList.remove(`resultado-invalido`)
+        resultadoInput.classList.add(`resultado-valido`)
+    } else {
+        resultadoInput.classList.remove(`resultado-valido`)
+        resultadoInput.classList.add(`resultado-invalido`)
+    };
+}
 
 function clickOnButton(event) {
     event.preventDefault()
@@ -116,11 +129,11 @@ function clickOnButton(event) {
     alturaN = Number(alturaInput.value);
 
     verificaDados()
-    calculaIMC()
     mostraResultado()
     adicionaOuRemoveEstilos()
     
 }
+
 
 
 
