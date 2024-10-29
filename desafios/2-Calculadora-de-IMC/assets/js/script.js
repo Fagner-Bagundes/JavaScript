af62e5868dd40a4e26037b5a255fd9571981b27d
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-29 14:44:49 -0300

    função que diz o estado físico do usuário feita

--- desafios/2-Calculadora-de-IMC/assets/js/script.js
+++ desafios/2-Calculadora-de-IMC/assets/js/script.js
@@ -1,22 +1,52 @@
 
-    
+
 const formulario = document.querySelector(`.formulario`)
 const btn = document.querySelector('.btn')
-const resultado = document.querySelector(`.resultado-calculo`)
+const resultadoInput = document.querySelector(`.resultado-calculo`)
 let pesoInput;
 let alturaInput;
 let pesoN;
 let alturaN;
+let valorIMC;
+let estadosR = [`Abaixo do peso`, `Peso Normal`, `Sobrepeso`, `Obesidade Grau 1`, `Obedidade Grau 2`, `Obesidade Grau 3`]
 
 
-function camposInvalidos() {
-    if (pesoN === 0 && alturaN === 0)  {
+function mostraResultado(){
+    if (valorIMC > 40) {
+        console.log(`Seu IMC é ${valorIMC} e você está: ${estadosR[5]}`)
+
+    }else if (valorIMC <= 39.9 && valorIMC>= 35){
+        console.log(`Seu IMC é ${valorIMC} e você está: ${estadosR[4]}`)
+
+    } else if(valorIMC <= 34.9 && valorIMC>= 30){
+        console.log(`Seu IMC é ${valorIMC} e você está: ${estadosR[3]}`)
+
+    } else if(valorIMC <= 29.9 && valorIMC >= 25){
+        console.log(`Seu IMC é ${valorIMC} e você está: ${estadosR[2]}`)
+
+    } else if(valorIMC<= 24.9 && valorIMC >= 18.5){
+        console.log(`Seu IMC é ${valorIMC} e você está: ${estadosR[1]}`)
+
+    } else {
+        console.log(`Seu IMC é ${valorIMC} e você está: ${estadosR[0]}`)
+    }
+}
+
+
+
+
+
+formulario.addEventListener(`submit`, clickOnButton)
+
+function verificaDados() {
+    if (pesoN === 0 && alturaN === 0) {
         console.log(`Digite alguma coisa nas caixas de textos`)
-    } else if( isNaN(alturaN) && isNaN(pesoN)){
+    } else if (isNaN(alturaN) && isNaN(pesoN)) {
         console.log(`Digite Numeros validos nos campos de textos`)
-    } else{
+    } else {
         alturaInvalida()
         pesoInvalido()
+        calculaIMC()
     }
 
 
@@ -24,25 +54,30 @@ function camposInvalidos() {
 
         if (isNaN(pesoN)) {
             console.log(`O valor de peso é inválido!!`);
-        } else if(pesoN >= 350){
+        } else if (pesoN >= 350) {
             console.log(`Digite um valor menor na area de peso`);
-    
+
         }
     }
-    
+
     function alturaInvalida() {
-    
+
         if (isNaN(alturaN)) {
             console.log(`O valor da altura é inválido!!`);
-        } else if(alturaN >= 3){
+        } else if (alturaN >= 3) {
             console.log(`Digite um valor menor na area de altura`);
-    
+
         }
     }
-    
-    
+
+
 }
 
+function calculaIMC() {
+    let calc = pesoN / (alturaN * alturaN)
+    valorIMC = calc.toFixed(1)
+    console.log(valorIMC)
+}
 
 
 function clickOnButton(event) {
@@ -53,13 +88,13 @@ function clickOnButton(event) {
     pesoN = Number(pesoInput.value)
     alturaN = Number(alturaInput.value)
 
-    ;
-    camposInvalidos()
+        ;
+    verificaDados()
+    mostraResultado()
 
-    
+
 }
 
 
-formulario.addEventListener(`submit`, clickOnButton)
 
 
