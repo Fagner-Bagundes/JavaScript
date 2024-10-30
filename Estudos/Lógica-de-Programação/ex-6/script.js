2702936fc353b4f56d90c14ce7f7ba7e02dcd835
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-30 11:58:30 -0300

    Formatando datas

--- Estudos/Lógica-de-Programação/ex-6/script.js
+++ Estudos/Lógica-de-Programação/ex-6/script.js
@@ -1,14 +1,37 @@
-const data = new Date()
-console.log(data.toString());
+const data1 = new Date()
+console.log(data1.toString());
 
 console.log(` `);
 
-console.log( `Ano`, data.getFullYear());
-console.log( `Mês`, data.getMonth() + 1);
-console.log( `dia`, data.getDate());
-console.log( `Hora`, data.getHours());
-console.log( `Minutos`, data.getMinutes());
-console.log( `Segundos`, data.getSeconds());
-console.log( `Milisegundos`, data.getMilliseconds());
-console.log( `Dia da Semana`, data.getDay() + 1);
+console.log( `Ano`, data1.getFullYear());
+console.log( `Mês`, data1.getMonth() + 1);
+console.log( `dia`, data1.getDate());
+console.log( `Hora`, data1.getHours());
+console.log( `Minutos`, data1.getMinutes());
+console.log( `Segundos`, data1.getSeconds());
+console.log( `Milisegundos`, data1.getMilliseconds());
+console.log( `Dia da Semana`, data1.getDay() + 1);
+
+console.log(`---------------------------------------------------------`)
+
+const data2 = new Date()
+
+function formataDatas(data2) {
+
+    let adiciona0 = (valor) => valor >= 10 ? valor : `0${valor}`
+
+    let datas = {
+        dia: adiciona0(data2.getDate()),
+        mes: adiciona0(data2.getMonth()),
+        ano: adiciona0(data2.getFullYear()),
+        hora: adiciona0(data2.getHours()),
+        min: adiciona0(data2.getMinutes()),
+        seg: adiciona0(data2.getSeconds()),
+    }    
+
+    console.log(`${datas.dia}/${datas.mes}/${datas.ano}  ${datas.hora}: ${datas.min}: ${datas.seg}`);
+    
+}
+
+formataDatas(data2)
 
