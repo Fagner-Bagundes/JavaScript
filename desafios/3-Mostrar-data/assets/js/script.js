8d1bd544271713cf6298d19e9bdc2d41d1e7a2d9
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-30 16:58:50 -0300

    tentando mostrar as datas de outra forma

--- desafios/3-Mostrar-data/assets/js/script.js
+++ desafios/3-Mostrar-data/assets/js/script.js
@@ -2,7 +2,37 @@ const modo1 = document.querySelector(`.modo1`)
 const modo2 = document.querySelector(`.modo2`)
 
 
+const dataAtual1 = new Date();
+let meses = [`Janeiro`,`Fevereiro`,`Março`,`Abril`,`Maio`,`Julho`,`Junho`,`Agosto`,`Setembro`,`Outubro`,`Novembro`,`Dezembro`]
+let dias = [`domingo`,`segunda`,`terça`,`quarta`,`quinta`,`sexta`,`sabado`,]
 
+
+let formataDia = (dia) => dia = meses[datas[dia]]
+
+let formataMes = (mes) => mes = meses[datas(mes)]
+
+
+
+
+
+let datas = (data) => {
+    const zero = (data) => data >= 10? data : `0data}` 
+
+    let dia = zero(dataAtual1.getDay())
+    let mes = zero(dataAtual1.getMonth())
+    let ano = zero(dataAtual1.getFullYear())
+    let hora = zero(dataAtual1.getHours())
+    let min = zero(dataAtual1.getMinutes())
+  
+
+    return [dia, mes, ano, hora, min]
+}
+
+console.log(datas);
+formataDia(datas[1]);
+formataMes(datas[2]);
+
+console.log(datas[1], datas[2]);
 
 
 
