340ab516b9e99ca4bb071947a9de71ece742e053
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-30 17:14:53 -0300

    formatação da data feita

--- desafios/3-Mostrar-data/assets/js/script.js
+++ desafios/3-Mostrar-data/assets/js/script.js
@@ -3,36 +3,29 @@ const modo2 = document.querySelector(`.modo2`)
 
 
 const dataAtual1 = new Date();
-let meses = [`Janeiro`,`Fevereiro`,`Março`,`Abril`,`Maio`,`Julho`,`Junho`,`Agosto`,`Setembro`,`Outubro`,`Novembro`,`Dezembro`]
-let dias = [`domingo`,`segunda`,`terça`,`quarta`,`quinta`,`sexta`,`sabado`,]
+let meses = [`Janeiro`, `Fevereiro`, `Março`, `Abril`, `Maio`, `Julho`, `Junho`, `Agosto`, `Setembro`, `Outubro`, `Novembro`, `Dezembro`];
+let dias = [`domingo`, `segunda`, `terça`, `quarta`, `quinta`, `sexta`, `sabado`,];
 
 
-let formataDia = (dia) => dia = meses[datas[dia]]
+let formataDia = (dia) => dataList[dia] = meses[dia];
+let formataMes = (mes) => dataList[mes] = meses[mes];
 
-let formataMes = (mes) => mes = meses[datas(mes)]
+const zero = (data) => data >= 10 ? data : `0${data}`;
+
+let dia = dataAtual1.getDay();
+let mes = dataAtual1.getMonth();
+let ano = zero(dataAtual1.getFullYear());
+let hora = zero(dataAtual1.getHours());
+let min = zero(dataAtual1.getMinutes());
+let dataList = [dia, mes, ano, hora, min];
+
+formataDia(0);
+formataMes(1);
+
+console.log(dataList[0]);
+console.log(dataList[1]);
 
 
-
-
-
-let datas = (data) => {
-    const zero = (data) => data >= 10? data : `0data}` 
-
-    let dia = zero(dataAtual1.getDay())
-    let mes = zero(dataAtual1.getMonth())
-    let ano = zero(dataAtual1.getFullYear())
-    let hora = zero(dataAtual1.getHours())
-    let min = zero(dataAtual1.getMinutes())
-  
-
-    return [dia, mes, ano, hora, min]
-}
-
-console.log(datas);
-formataDia(datas[1]);
-formataMes(datas[2]);
-
-console.log(datas[1], datas[2]);
 
 
 
@@ -42,7 +35,7 @@ const dataAtual2 = new Date();
 const opcoes = {
     dateStyle: 'full',
     timeStyle: 'short'
-}
+};
 
-modo2.innerHTML = dataAtual2.toLocaleString('pt-BR', opcoes)
+modo2.innerHTML = dataAtual2.toLocaleString('pt-BR', opcoes);
 // Saída: "quarta-feira, 30 de outubro de 2024"
