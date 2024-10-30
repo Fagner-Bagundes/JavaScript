0e0224da751ef76a1a384035c0bf1c232221f973
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-30 18:23:50 -0300

    entendendo atribuição via desestruturação com arrays

---
+++ Estudos/Lógica-de-Programação/ex-10/script.js
@@ -0,0 +1,19 @@
+// Reatribuição via desestruturação
+
+let a = `A`;
+let b = `B`;
+let c = `C`;
+
+[a, b, c] = [b,c,a]
+
+console.log(a,b,c)
+
+console.log(`-----------------------------------------------------------`);
+let num1;
+let num2;
+let num3;
+
+[num1, num2, num3] = [10, 20, 30]
+console.log(num1, num2, num3);
+
+
