6b6347e87345412de6a40962dc3120f21c276c04
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-30 17:44:36 -0300

    deixando o código como menos linhas

--- desafios/3-Mostrar-data/assets/js/script.js
+++ desafios/3-Mostrar-data/assets/js/script.js
@@ -1,3 +1,5 @@
+
+// Primeiro modo de fazer, mais manual
 const modo1 = document.querySelector(`.modo1`)
 
 const dataAtual1 = new Date();
@@ -6,27 +8,24 @@ let dias = [`domingo`, `segunda`, `terça`, `quarta`, `quinta`, `sexta`, `sabado
 
 const zero = (data) => data >= 10 ? data : `0${data}`;
 
-let dia = dataAtual1.getDay();
-let diaMes = zero(dataAtual1.getDate());
-let mes = dataAtual1.getMonth();
-let ano = zero(dataAtual1.getFullYear());
-let hora = zero(dataAtual1.getHours());
-let min = zero(dataAtual1.getMinutes());
-let datasList = [dia, mes, ano, hora, min, diaMes];
+const dia = dataAtual1.getDay();
+const diaMes = zero(dataAtual1.getDate());
+const mes = dataAtual1.getMonth();
+const ano = zero(dataAtual1.getFullYear());
+const hora = zero(dataAtual1.getHours());
+const min = zero(dataAtual1.getMinutes());
 let diaFormatado;
 let mesFormatado;
 
-
 function formatacao(dia, mes) {
-    diaFormatado = dias[datasList[dia]];
-    mesFormatado = meses[datasList[mes]];
+    diaFormatado = dias[dia];
+    mesFormatado = meses[mes];
 }
 
-formatacao(0, 1);
+formatacao(dia, mes);
 modo1.innerHTML = `${diaFormatado}, ${diaMes} de ${mesFormatado} de ${ano} ás ${hora}:${min}`
 
-
-
+// Segundo modo de fazer mais automatizado:
 const modo2 = document.querySelector(`.modo2`)
 const dataAtual2 = new Date();
 const opcoes = {
