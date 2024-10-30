33e8f17f2fef52909bdac2882610f5047a2fcf26
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-30 17:36:39 -0300

    medo 1 de fazer completo

--- desafios/3-Mostrar-data/assets/js/script.js
+++ desafios/3-Mostrar-data/assets/js/script.js
@@ -1,36 +1,33 @@
 const modo1 = document.querySelector(`.modo1`)
-const modo2 = document.querySelector(`.modo2`)
-
 
 const dataAtual1 = new Date();
 let meses = [`Janeiro`, `Fevereiro`, `Março`, `Abril`, `Maio`, `Julho`, `Junho`, `Agosto`, `Setembro`, `Outubro`, `Novembro`, `Dezembro`];
 let dias = [`domingo`, `segunda`, `terça`, `quarta`, `quinta`, `sexta`, `sabado`,];
 
-
-let formataDia = (dia) => dataList[dia] = meses[dia];
-let formataMes = (mes) => dataList[mes] = meses[mes];
-
 const zero = (data) => data >= 10 ? data : `0${data}`;
 
 let dia = dataAtual1.getDay();
+let diaMes = zero(dataAtual1.getDate());
 let mes = dataAtual1.getMonth();
 let ano = zero(dataAtual1.getFullYear());
 let hora = zero(dataAtual1.getHours());
 let min = zero(dataAtual1.getMinutes());
-let dataList = [dia, mes, ano, hora, min];
-
-formataDia(0);
-formataMes(1);
-
-console.log(dataList[0]);
-console.log(dataList[1]);
+let datasList = [dia, mes, ano, hora, min, diaMes];
+let diaFormatado;
+let mesFormatado;
 
 
+function formatacao(dia, mes) {
+    diaFormatado = dias[datasList[dia]];
+    mesFormatado = meses[datasList[mes]];
+}
+
+formatacao(0, 1);
+modo1.innerHTML = `${diaFormatado}, ${diaMes} de ${mesFormatado} de ${ano} ás ${hora}:${min}`
 
 
 
-
-
+const modo2 = document.querySelector(`.modo2`)
 const dataAtual2 = new Date();
 const opcoes = {
     dateStyle: 'full',
