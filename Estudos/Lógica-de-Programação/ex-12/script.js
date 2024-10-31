7e61677ee7e4e5e23f5f5bfc1a2f2a164749c712
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-31 16:37:46 -0300

    percorrendo uma array com for

--- Estudos/Lógica-de-Programação/ex-12/script.js
+++ Estudos/Lógica-de-Programação/ex-12/script.js
@@ -1,4 +1,13 @@
 for(let i = 0; i <=5; i++) {
     console.log(i,` Hello Wolrd`);
-    
+}
+
+console.log(` `);
+
+const lista = [`açai`, `mostadela`,`Feijão`, `calabresa`, `toscana`];
+console.log(`Lista de compras`)
+
+for(let i=0; i<lista.length; i++){
+    console.log(i,`-`,lista[i])
+
 }
