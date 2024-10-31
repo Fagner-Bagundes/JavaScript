8da442181b1f8ce1011a166e46177d9c022f0dcc
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-31 17:17:46 -0300

    pegando propriedades de objetos

--- desafios/4- criando-divs-no HTML-com-JS/assets/js/script.js
+++ desafios/4- criando-divs-no HTML-com-JS/assets/js/script.js
@@ -1,6 +1,20 @@
 const lista = [
-    {tag: `h1`, txt: `Frase 1`},
-    {tag: `section`, txt: `Frase 2`},
-    {tag: `div`, txt: `Frase 3`},
-    {tag: `p`, txt: `Frase 4`},
+    {tag: `h1`, txt: `Era uma vez`},
+    {tag: `section`, txt: `Um guerreiro da indonésia`},
+    {tag: `div`, txt: `que tinha 4 braços`},
+    {tag: `p`, txt: `Seu nome era shiva`},
 ]
+
+// for (let i=0; i<lista.length; i++){
+
+// }
+
+
+let [tag1, tag2, tag3, tag4] = lista;
+
+let {tag: h1, txt: frase1} = tag1;
+let {tag: section, txt: frase2} = tag2;
+let {tag: div, txt: frase3} = tag3;
+let {tag: p, txt: frase4} = tag4;
+
+console.log( h1, section, div, p, frase2);
