b1460a7324a19439b5b211fa7568531fbf003b89
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-31 16:50:22 -0300

    dizendo se se os numero numero de 1 a 10 são par ou impar

--- Estudos/Lógica-de-Programação/ex-12/script.js
+++ Estudos/Lógica-de-Programação/ex-12/script.js
@@ -4,10 +4,23 @@ for(let i = 0; i <=5; i++) {
 
 console.log(` `);
 
+
+// percorrendo uma Array com for
 const lista = [`açai`, `mostadela`,`Feijão`, `calabresa`, `toscana`];
 console.log(`Lista de compras`)
 
 for(let i=0; i<lista.length; i++){
     console.log(i,`-`,lista[i])
+};
+
+console.log(` `)
+// Dizendo se um numero é impar ou par com for
+console.log(`Dizendo se cada numero de 1 a 10 é impar ou par com for
+`)
+
+for(let i=1; i<=10; i++){
+    const imparOuPar = i % 2 === 0 ? `par` : `impar`; 
+    console.log(`O numero ${i} é: ${imparOuPar}`)
 
 }
+
