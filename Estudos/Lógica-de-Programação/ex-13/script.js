587404a895e788f2bc44f706fa6e412fd132db0c
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-01 11:11:41 -0300

    percorrendo um objeto com for in

--- Estudos/Lógica-de-Programação/ex-13/script.js
+++ Estudos/Lógica-de-Programação/ex-13/script.js
@@ -1,7 +1,20 @@
-// Percorrendo um array com for
+// Percorrendo um array com for in
+console.log(` percorrendo uma array com for in`);
 const frutas = [`maça`, `pera`, `uva`]
 
 for (let indice in frutas){
     console.log(indice, frutas[indice]);
-    
+}
+
+// Percorrendo um objeto com for in
+console.log(` percorrendo um objeto com for in`);
+
+const pessoa = {
+    nome: `Fagner`,
+    sobrenome: `Ferreira`,
+    idade: 10
+}
+
+for ( let chave in pessoa){
+    console.log(`${chave}: ${pessoa[chave]}`)
 }
