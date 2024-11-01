8e26eca673f6406d3a5eb8125e09fd363351b149
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-01 11:40:34 -0300

    entendendo o for of e comparando com for clássico e for in

---
+++ Estudos/Lógica-de-Programação/ex-14/script.js
@@ -0,0 +1,31 @@
+const nome = `Fagner Ferreira`;
+
+// mostrando o valor da variavel com for clássico
+for (let i=0; i<nome.length; i++) {
+    console.log(nome[i])
+}
+
+console.log(`---------------------------------------------`);
+
+// mostrando o valor da variavel com for in
+
+for(let i in nome){
+    console.log(nome[i]);
+}
+
+
+console.log(`---------------------------------------------`);
+// mostrando o valor da variavel com for of
+for (let valor of nome){
+    console.log(valor); 
+}
+
+
+
+
+
+console.log(`---------------------------------------------`);
+
+
+
+console.log(`---------------------------------------------`);
