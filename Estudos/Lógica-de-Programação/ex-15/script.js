10c1c6304a66599e515e0b83bdabd8b8fd73bbe0
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-01 16:07:02 -0300

    entendendo do while

--- Estudos/Lógica-de-Programação/ex-15/script.js
+++ Estudos/Lógica-de-Programação/ex-15/script.js
@@ -4,11 +4,16 @@ function random(min, max){
 
 }
 
-let NumRandom = random(1, 20)
+let NumRandom = random(1, 11)
 
 while (NumRandom !== 10){
-    NumRandom = random(1,20)
+    NumRandom = random(1,11)
     console.log(NumRandom)
 }
+console.log(`-------------------------------------------`);
 
 
+do{
+    NumRandom = random(1,11)
+    console.log(NumRandom)
+}while(NumRandom !== 10)
