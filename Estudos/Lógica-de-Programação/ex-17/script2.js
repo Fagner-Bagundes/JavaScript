67e1e6abbd153cdeea82f05a5e57dead1d96dc48
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-03 14:22:37 -0300

    tratando erros com tudo q foi aprendido

--- Estudos/Lógica-de-Programação/ex-17/script2.js
+++ Estudos/Lógica-de-Programação/ex-17/script2.js
@@ -8,5 +8,31 @@ try {
 } finally{
     console.log(`Fechei arquivo`);
     console.log(`FINALLY: Eu sempre sou executado`);
-    
 }
+
+console.log(`------------------------------------------------------`);
+
+
+function retornaHora(data) {
+    if (data && !(data instanceof Date)) {
+        throw new TypeError("Esperando instância de Date");
+    }
+
+    if (!data) data = new Date();
+    
+    return data.toLocaleTimeString(`pt-BR`)
+}
+
+
+try{
+    const date = new Date(`08-06-2005 01:30:00` )
+    const hora = retornaHora(date);
+    console.log(hora);
+} catch(error){
+    // tratando error
+    console.log(`Foi encontrado um erro, pois não foi mandado um objeto do tipo Date`);
+    
+} finally{
+    console.log(`tenha um bom dia`)
+}
+
