0c71005d2187b013f817fbc7010c2a5b034d4fc1
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-02 22:34:29 -0300

    entendendo try , catch e throw

---
+++ Estudos/Lógica-de-Programação/ex-17/script.js
@@ -0,0 +1,30 @@
+// // entendendo o try
+
+// try{
+//     console.log(naoExisto);
+// } catch(erro) {
+//     console.log(`Variavel nao existe`);
+//     console.log(erro);
+    
+// }
+
+
+function soma(x,y) {
+    if (
+      typeof x !== `number` ||
+      typeof y !== `number`
+    ){
+        throw(`x e y precisam ser números`)
+    }
+    return x + y;
+};
+
+try {
+    console.log(soma(1,5));
+    console.log(soma(3,`fd `));
+    
+} catch(error){
+    console.log(error);
+    
+}
+
