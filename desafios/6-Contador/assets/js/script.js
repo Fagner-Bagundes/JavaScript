043afca64ddb9b1acd846230378001a8dbdcf150
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-03 18:18:35 -0300

    programação feita para iniciar o timer finalizada

--- desafios/6-Contador/assets/js/script.js
+++ desafios/6-Contador/assets/js/script.js
@@ -2,39 +2,82 @@ const relogio = document.querySelector(`.relogio`)
 const btnIniciar = document.querySelector(`.iniciar`);
 const btnPausar = document.querySelector(`.pausar`);
 const btnZerar = document.querySelector(`.zerar`);;
-// let hora = new Date().toLocaleTimeString(`pt-BR`)
-let seg = 0;
+
+let hr = 0;
 let min = 0;
-let hora = 0;
+let seg = 0;
+
+let horario;
 
 
-function adicionaSegundos(){
-    let segundos = setInterval(()=>{
-        if (seg<=5){
-            console.log(seg);
-            ++seg
-        } else {
-            seg = 0
-           
-        }
-        
-    }, 1000)
-   
+function atualizaHoario(){
+    horario = new Date(`10/10/2005 ${hr}:${min}:${seg}`).toLocaleTimeString(`pt-BR`);
+    relogio.innerHTML = horario
 }
 
+function adicionaSegundos() {
+    
+    let segundos = setInterval(() => {
+            console.log(seg);
+            ++seg
+            atualizaHoario();
+            if(seg === 60) seg = 0 
+
+        
+    }, 1000)
+};
 
 
-btnIniciar.addEventListener(`click`, (event)=>{
+
+function adicionaMinutos() {
+    
+    let minutos = setInterval(() => {
+            console.log(min);
+            ++min
+            atualizaHoario();
+            if(min === 60) min = 0 
+
+        
+    }, 10000)
+};
+
+
+function adicionaHoras() {
+    
+    let horas = setInterval(() => {
+            console.log(hr);
+            ++hr
+            atualizaHoario();
+            if(hr === 60) hr = 0 
+
+        
+    }, 100000)
+};
+
+
+
+
+
+
+
+
+btnIniciar.addEventListener(`click`, (event) => {
     console.log(`Botão iniciar clicaco`)
     adicionaSegundos();
+    adicionaMinutos();
+    adicionaHoras()
+
+    
+    
+
 })
 
-btnPausar.addEventListener(`click`, (event)=>{
+btnPausar.addEventListener(`click`, (event) => {
     console.log(`Botão pausar clicado`)
 })
 
-btnZerar.addEventListener(`click`, (event)=>{
+btnZerar.addEventListener(`click`, (event) => {
     console.log(`Botão zerar clicado`)
-    clearInterval(segundos);
+    horario = new Date(`00:00:00`).toLocaleTimeString(`pt-BR`)
 
 })
