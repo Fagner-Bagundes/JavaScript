cece727fd256eceefc5eb3a9c09b106858adbeea
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-03 14:40:33 -0300

    entendendo setInterval

---
+++ Estudos/Lógica-de-Programação/ex-18/script.js
@@ -0,0 +1,13 @@
+function mostraHora(){
+    let data = new Date();
+    
+    return data.toLocaleTimeString(`pt-BR`)
+}
+
+
+function funcaoDoInterval(){
+    console.log(mostraHora());
+    
+}
+setInterval(funcaoDoInterval, 1000);
+
