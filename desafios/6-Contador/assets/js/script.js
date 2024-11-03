5bc12288e0ab9b9b6e65411cb2f15f23ff058004
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-03 19:10:06 -0300

    fazendo algumas melhorias no projeto

--- desafios/6-Contador/assets/js/script.js
+++ desafios/6-Contador/assets/js/script.js
@@ -2,6 +2,7 @@ const relogio = document.querySelector(`.relogio`)
 const btnIniciar = document.querySelector(`.iniciar`);
 const btnPausar = document.querySelector(`.pausar`);
 const btnZerar = document.querySelector(`.zerar`);;
+const divAviso = document.querySelector(`.aviso`)
 
 let hrContador;
 let minContador;
@@ -10,78 +11,70 @@ let segContador;
 let hr = 0;
 let min = 0;
 let seg = 0;
-let horario;
 
 
-function atualizaHoario() {
-    horario = new Date(`10/10/2005 ${hr}:${min}:${seg}`).toLocaleTimeString(`pt-BR`);
-    relogio.innerHTML = horario
-}
+
+
+
+function adicionaHoras() {
+    hrContador = setInterval(() => {
+        ++hr
+        atualizaHoario();
+        if (hr === 60) hr = 0
+    }, 3600000)
+};
+
+function adicionaMinutos() {
+    minContador = setInterval(() => {
+        ++min
+        atualizaHoario();
+        if (min === 60) min = 0
+    }, 60000)
+};
 
 function adicionaSegundos() {
-
     segContador = setInterval(() => {
-        console.log(seg);
         ++seg
         atualizaHoario();
         if (seg === 60) seg = 0
-
-
     }, 1000)
 };
 
 
-
-function adicionaMinutos() {
-
-    minContador = setInterval(() => {
-        console.log(min);
-        ++min
-        atualizaHoario();
-        if (min === 60) min = 0
+function atualizaHoario() {
+    relogio.innerHTML = new Date(`10/10/2005 ${hr}:${min}:${seg}`).toLocaleTimeString(`pt-BR`);
+}
 
 
-    }, 60000)
-};
-
-
-function adicionaHoras() {
-
-    hrContador = setInterval(() => {
-        console.log(hr);
-        ++hr
-        atualizaHoario();
-        if (hr === 60) hr = 0
-
-
-    }, 600000)
-};
-
-
-
-
-
-
-
+let contadorDeClicks = 0;
 
 btnIniciar.addEventListener(`click`, (event) => {
-    console.log(`Botão iniciar clicaco`)
-    adicionaSegundos();
-    adicionaMinutos();
-    adicionaHoras()
-
+    contadorDeClicks = ++contadorDeClicks
+    if (contadorDeClicks < 2){
+        divAviso.innerHTML = `O timer está rodando...`
+        adicionaSegundos();
+        adicionaMinutos();
+        adicionaHoras()
+    } else {
+        divAviso.innerHTML = `para iniciar um novo timer, voce tem que zerar o antigo!!`
+        divAviso.classList.remove(`aviso`)
+        divAviso.classList.add(`avisoError`)
+    }
 
 })
 
 btnPausar.addEventListener(`click`, (event) => {
     console.log(`Botão pausar clicado`)
+    contadorDeClicks = 0;
     clearInterval(segContador);
     clearInterval(minContador);
     clearInterval(hrContador);
+    divAviso.innerHTML = `O timer está está Pausado🥱`
 })
 
 btnZerar.addEventListener(`click`, (event) => {
     console.log(`Botão zerar clicado`)
+    contadorDeClicks = 0;
     clearInterval(segContador);
     clearInterval(minContador);
     clearInterval(hrContador);
@@ -89,4 +82,8 @@ btnZerar.addEventListener(`click`, (event) => {
     min = 0
     seg = 0
     atualizaHoario()
+    divAviso.innerHTML = `Aperte inciar para começar o timer😁`;
+    divAviso.classList.remove(`avisoError`)
+    divAviso.classList.add(`aviso`)
+    
 })
