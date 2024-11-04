486548baf4d012c85a717e880590f8558a9440a6
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-04 17:40:58 -0300

    projeto refeito

--- Estudos/Lógica-de-Programação/tests/ex1/assets/js/script.js
+++ Estudos/Lógica-de-Programação/tests/ex1/assets/js/script.js
@@ -6,72 +6,78 @@ let peso;
 let altura;
 let IMC;
 
-function mostraErros(error){
+function mostraErros(error) {
     resutadoCalculo.innerHTML = error
 }
 
-function mostraIMC(estadoFisico){
+function mostraIMC(estadoFisico) {
     resutadoCalculo.innerHTML = `Seu IMC é: ${IMC} e o seu estado é: ${estadoFisico} `
 }
 
 function calculaIMC(params) {
     IMC = peso / (altura * altura);
     IMC = IMC.toFixed(2);
-    carrega();
-    
-    if(IMC > 40) mostraIMC(`Obesidade Grau 3`);
-    if(IMC>35 && IMC<39.9) mostraIMC(`Obesidade Grau 2`);
-    if(IMC>30 && IMC<34.9) mostraIMC(`Obesidade Grau 1`);
-    if(IMC>25 && IMC<29.9) mostraIMC(`Sobrepeso`);
-    if(IMC>18.5 && IMC<24.9) mostraIMC(`Peso Normal`);
-    if(IMC<18.5) mostraIMC(`Abaixo do peso`);
+
+
+    if (IMC > 40) mostraIMC(`Obesidade Grau 3`);
+    if (IMC > 35 && IMC < 39.9) mostraIMC(`Obesidade Grau 2`);
+    if (IMC > 30 && IMC < 34.9) mostraIMC(`Obesidade Grau 1`);
+    if (IMC > 25 && IMC < 29.9) mostraIMC(`Sobrepeso`);
+    if (IMC > 18.5 && IMC < 24.9) mostraIMC(`Peso Normal`);
+    if (IMC < 18.5) mostraIMC(`Abaixo do peso`);
 
 }
 
-function filtraDados(){
-    if (peso === 0 && altura === 0){
-        mostraErros(`Digite Valores Válidos`);  
-    }  else if (isNaN(peso) && isNaN(altura)){
+function filtraDados() {
+    if (peso === 0 && altura === 0) {
+        mostraErros(`Digite Valores Válidos`);
+    } else if (isNaN(peso) && isNaN(altura)) {
         mostraErros(`Digite Valores Válidos`)
         return
     } else if (isNaN(peso)) {
         mostraErros(`O valor do peso é invalido`);
-    } else if (isNaN(altura)){
+    } else if (isNaN(altura)) {
         mostraErros(`O valor da altura é invalido`)
-    } else {
-        calculaIMC()
+    } else if(contadorClick === 0){
+        carrega();
     }
 };
 
-formulario.addEventListener(`submit`, (e)=>{
+formulario.addEventListener(`submit`, (e) => {
     e.preventDefault();
     peso = Number(pesoInput.value);
     altura = Number(alturaInput.value);
     console.log(peso, altura)
     filtraDados();
- 
-    calculaIMC();
-    
 
-    
+
+
+
 });
 
 
+let contadorClick = 0;
 function carrega() {
-    let adicionaPonto = `fazendo calculo` 
-    let timer
+    let adicionaPonto = `fazendo calculo`
+    let timer;
 
-        timer = setInterval(()=>{
-            console.log(adicionaPonto.length);
-                resutadoCalculo.innerHTML = adicionaPonto
-                adicionaPonto += `.`  
-            
-        }, 300)
 
-        setTimeout(() => {
-            clearInterval(timer)
-        }, 2000);
-  
+
+    timer = setInterval(() => {
+        contadorClick = 1;
+        console.log(contadorClick);
+
+        resutadoCalculo.innerHTML = adicionaPonto
+        adicionaPonto += `.`
+
+    }, 300)
+
+    setTimeout(() => {
+        clearInterval(timer)
+        calculaIMC()
+        contadorClick = 0;
+    }, 2000);
+
 }
 
 
