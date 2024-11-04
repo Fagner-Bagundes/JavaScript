6e233d08965a3b12245f6d7a11a5b2937b2c252b
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-04 16:22:11 -0300

    refazendo a calculadora imc

--- Estudos/Lógica-de-Programação/tests/ex1/assets/js/script.js
+++ Estudos/Lógica-de-Programação/tests/ex1/assets/js/script.js
@@ -1,17 +1,28 @@
 const formulario = document.querySelector(`.formulario`);
 const pesoInput = document.querySelector(`.peso-input`);
 const alturaInput = document.querySelector(`.altura-input`);
+const resutadoCalculo = document.querySelector(`.resultado-calculo`)
 let peso;
 let altura;
 
-
+function mostraErros(error){
+    resutadoCalculo.innerHTML = error
+}
 
 function filtraDados(){
-    if (!peso || !altura) {
-        console.log(`error`);
-        
-    }
-}
+    if (peso === 0 && altura === 0){
+        mostraErros(`Digite Valores Válidos`);  
+    } 
+    if (isNaN(peso) && isNaN(altura)){
+    mostraErros(`Digite Valores Válidos`)
+    return
+    } 
+    
+    if (isNaN(peso)) mostraErros(`O valor do peso é invalido`);
+    if (isNaN(altura)) mostraErros(`O valor da altura é invalido`)
+
+     
+};
 
 
 
@@ -20,7 +31,7 @@ formulario.addEventListener(`submit`, (e)=>{
     e.preventDefault();
     peso = Number(pesoInput.value);
     altura = Number(alturaInput.value);
-    console.log(peso)
+    console.log(peso, altura)
     filtraDados();
 });
 
