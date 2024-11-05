dda44b045878bc16626c761a73a79c2443870fb3
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-05 16:44:26 -0300

    entendendo funções  que retornam funções

--- Estudos/3-Funções-avançado/3-retorno-de-funcao/script.js
+++ Estudos/3-Funções-avançado/3-retorno-de-funcao/script.js
@@ -4,10 +4,34 @@
 
 
 function soma(a, b){
-   return b * b;
+   return a * b;
    console.log(`não aparecerei`);
    
 }
 
 let resultado = soma(9,9)
 console.log(resultado);
+
+function criaPessoa(nome, sobrenome) {
+    return {
+        nome: nome, 
+        sobrenome: sobrenome
+    }
+}
+
+const p1 = criaPessoa(`Fagner`, `Ferreira`)
+console.log(p1);
+
+
+
+
+function criaMultiplicador(multiplicador){
+     let multiplica = (numero)=>{
+        return numero * multiplicador
+    }
+    return multiplica;
+}
+
+let duplica = criaMultiplicador(2)
+
+console.log(duplica(5));
