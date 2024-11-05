013d504d93bd18e8d4e430b07de76c96dfa24c28
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-05 13:31:20 -0300

    entendendo atribuição via desestruturação com parâmetros

--- Estudos/3-Funções-avançado/2-parametros/script.js
+++ Estudos/3-Funções-avançado/2-parametros/script.js
@@ -18,7 +18,13 @@
 // }
 
 // funcao3(1,2,3,4,5,6)
+let barra = ()=> console.log(`-----------------------------------------------------`);
+let espaco = ()=> console.log(` `);
 
+
+
+console.log(`Usando Arguments de funções:`);
+espaco()
 
 function funcao1(nome, sobrenome){
     console.log(nome, sobrenome);
@@ -27,5 +33,25 @@ function funcao1(nome, sobrenome){
     console.log(arguments[2]);
     
 }
-
 funcao1(`Fagner`, `Ferreira`, `Bagundes`, 19)
+barra()
+console.log(`Atribuição via desestruturação em parâmetros com array:`);
+espaco()
+
+function funcao2([fruta1, fruta2, fruta3]){
+    console.log(fruta1, fruta2, fruta3);
+    
+}
+
+funcao2([`Açai`, `maça`, `uva`])
+barra()
+
+console.log(`Atribuição via desestruturação em parâmetros com obejetos:`);
+espaco()
+function fucao3({fruta1, fruta2, fruta3}){
+    console.log(fruta1, fruta2,fruta3);
+}
+
+fucao3({fruta1: `maça`,fruta2: `pera`,fruta3: `jaca` })
+
+
