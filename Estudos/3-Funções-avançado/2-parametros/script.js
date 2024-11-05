f1e9c22ab91e5ef45fce0a8e741f5fe495c734fb
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-05 13:23:22 -0300

    entendendo a variável arguments de funções

--- Estudos/3-Funções-avançado/2-parametros/script.js
+++ Estudos/3-Funções-avançado/2-parametros/script.js
@@ -1,20 +1,31 @@
-function funcao(mensagem) {
-    console.log(mensagem);
+// function funcao(mensagem) {
+//     console.log(mensagem);
+    
+// }
+
+// funcao(`oiii`);
+
+
+// function funcao2({a, b, c}){
+//     console.log(a, b, c);
+// }
+
+// funcao2({a:1 ,b:2 ,c:3})
+
+
+// function funcao3(valor1, valor2, valor3, ...resto){
+//     console.log(valor1, valor2, valor3, resto);
+// }
+
+// funcao3(1,2,3,4,5,6)
+
+
+function funcao1(nome, sobrenome){
+    console.log(nome, sobrenome);
+    
+    console.log(arguments);
+    console.log(arguments[2]);
     
 }
 
-funcao(`oiii`);
-
-
-function funcao2({a, b, c}){
-    console.log(a, b, c);
-}
-
-funcao2({a:1 ,b:2 ,c:3})
-
-
-function funcao3(valor1, valor2, valor3, ...resto){
-    console.log(valor1, valor2, valor3, resto);
-}
-
-funcao3(1,2,3,4,5,6)
+funcao1(`Fagner`, `Ferreira`, `Bagundes`, 19)
