75e6e339c77717dd5255753f35202e96068e405a
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-05 15:38:16 -0300

    entendendo o return

---
+++ Estudos/3-Funções-avançado/3-retorno-de-funcao/script.js
@@ -0,0 +1,13 @@
+// Return
+// retorna um valor
+// encerra a função
+
+
+function soma(a, b){
+   return b * b;
+   console.log(`não aparecerei`);
+   
+}
+
+let resultado = soma(9,9)
+console.log(resultado);
