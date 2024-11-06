2b2fe4df2ce7cab9e4b6aeedc3563eccd80359f5
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-06 19:00:27 -0300

    entendendo o getter e o setter

--- Estudos/3-Funções-avançado/7-factory-functions/script.js
+++ Estudos/3-Funções-avançado/7-factory-functions/script.js
@@ -1,18 +1,41 @@
-function criaPessoa(nome, sobrenome, idade){
+function criaPessoa(nome, sobrenome, idade, altura, peso){
     return {
         nome,
         sobrenome,
         idade,
-        
+        altura,
+        peso,
         
         falaOi(frase){
-            return `a pessoa ${this.nome} ${this.sobrenome} tem ${this.idade} anos e está falando que ${frase}`
+            return `a pessoa ${this.nome} ${this.sobrenome} tem ${this.idade} anos e está falando que ${frase} e seu imc é: ${this.IMC}`
+        },
+
+        get IMC(){
+            const indice = this.peso / (this.altura ** 2)
+            return indice.toFixed(2)
+        },
+
+        // getter
+        get nomeCompleto(){
+           return this.nome +` `+  this.sobrenome
+        },
+        // setter
+        set nomeCompleto(nome){
+            nome = nome.split(` `);
+            console.log(nome);
+            
         }
+
     }
 }
 
 
-let p1 = criaPessoa(`Fagner`, `Ferreira`, 19)
-let p2 = criaPessoa(`Francisco`, `Santana`, 57)
+let p1 = criaPessoa(`Fagner`, `Ferreira`, 19, 1.65, 58)
 
-console.log(p1.falaOi(`eu quero é ser dev pleno`), p2.falaOi(`o trump é maligno`));
+let p2 = criaPessoa(`Francisco`, `Santana`, 57, 1.65, 90)
+
+console.log(p1.falaOi(`eu quero é ser dev pleno`));
+console.log(`----------------------------------------------------`);
+p1.nomeCompleto = `Nalda Maria`
+console.log(p1.nomeCompleto);
+
