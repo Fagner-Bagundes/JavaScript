e640984a2c178d8ab45b0c2d3263f7c07008b67c
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-08 15:56:30 -0300

    aprendendo sobre valores privados

--- Estudos/3-Funções-avançado/8-funcoes-construtoras/script.js
+++ Estudos/3-Funções-avançado/8-funcoes-construtoras/script.js
@@ -1,11 +1,18 @@
 function Pessoa(nome, sobrenome){
+    const ID = 1234; //variável privada
+    
     this.nome = nome;
     this.sobrenome = sobrenome;
+
+    this.metodo = ()=>{
+        console.log(this.nome + ` está falando oi`);
+    }
 
 }
 
 
 const p1 = new Pessoa(`Fagner`, `Ferreira`);
 
-console.log(p1);
+
+p1.metodo();
 
