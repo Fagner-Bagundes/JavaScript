0a4556ef817e21c1cb8ed5f371333f9b76f38315
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-09 15:45:28 -0300

    funções geradoras infinitas

--- Estudos/3-Funções-avançado/10-funcoes-geradoras/script.js
+++ Estudos/3-Funções-avançado/10-funcoes-geradoras/script.js
@@ -8,8 +8,38 @@ function* geradora1() {
     //  código qualquer...
 };
 
+
 const g1 = geradora1();
+
 console.log(g1.next().value);
 console.log(g1.next().value);
 console.log(g1.next().value);
 console.log(g1.next());
+
+console.log(`--------------------------------------------`);
+
+// funções geradoras infinitas:
+
+function* geradora2(){
+    let i = 0;
+
+    while(true){
+        yield i;
+        i++
+    }
+}
+
+let g2 = geradora2();
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+
+
+
+
+
+
+
+
+
