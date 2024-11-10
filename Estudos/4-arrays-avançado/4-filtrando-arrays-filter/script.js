42da069f7c2d557deb7c80986df4d9ced97a24b4
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-10 19:03:15 -0300

    entendendo os parâmetros de filter

--- Estudos/4-arrays-avançado/4-filtrando-arrays-filter/script.js
+++ Estudos/4-arrays-avançado/4-filtrando-arrays-filter/script.js
@@ -2,11 +2,16 @@
 // função filter querer que retorner um boleano
 
 // retorna os numeros maiores que 10
-const numeros = [5,50,80,1,2,3,5,8,7,11,15,22,27]
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
 
 
-const numerosFiltrados = numeros.filter((valor) => {
-    return valor>10
+const numerosFiltrados = numeros.filter((valor, indice, array) => {
+    console.log(valor);
+    console.log(indice);
+    console.log(array);
+    
+    
+    return valor > 10
 });
 console.log(numerosFiltrados);
 
