60da6ddb9d6ac2060729bc1a51559505683e22f8
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-11 15:05:48 -0300

    fazendo tudo em uma única expressão

--- Estudos/4-arrays-avançado/7-filter+map+reduce/script.js
+++ Estudos/4-arrays-avançado/7-filter+map+reduce/script.js
@@ -8,22 +8,40 @@ console.log(` `);
 
 const numeros = [1,4,35,6,17,3,9,58,90,10]
 console.log(`Array original:`);
+
 console.log(numeros);
+
 console.log(` `);
 
 console.log(`Filtrando os pares:`);
+
 const pares = numeros.filter((num)=> num % 2 === 0)
 console.log(pares);
+
 console.log(` `);
 
 console.log(`Dobrando os valores dos pares:`);
+
 let dobro = pares.map((num)=> num *2)
 console.log(dobro);
+
 console.log(` `);
 
 console.log(`Somando todos os valores:`);
 let soma = dobro.reduce((acumulador, valor)=> acumulador + valor)
 console.log(soma);
+
+// colocando tudo em uma única expressão
+console.log(`Fazendo tudo em uma única expressão:`);
+
+let ValorFinal = numeros
+.filter((valor)=> valor % 2 ===0)
+.map((valor)=> valor *2)
+.reduce((ac, valor)=> ac + valor)
+
+
+console.log(ValorFinal);
+
 
 
 
