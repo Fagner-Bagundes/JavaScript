d490e83e97e8aaa3749b5a725883eb9d791dd6cc
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-11 14:52:24 -0300

    trabalhondo com map, filter e reduce

--- Estudos/4-arrays-avançado/7-filter+map+reduce/script.js
+++ Estudos/4-arrays-avançado/7-filter+map+reduce/script.js
@@ -1,1 +1,30 @@
-console.log(`Unindo Filter, Map,`);
+console.log(`Unindo Filter, Map e Reduce`);
+console.log(` `);
+
+// Retorne a soma do dobro de todos os pares
+// -> Filtrar pares
+// -> Dobrar os valores
+// -> Reduzir (somar tudo)
+
+const numeros = [1,4,35,6,17,3,9,58,90,10]
+console.log(`Array original:`);
+console.log(numeros);
+console.log(` `);
+
+console.log(`Filtrando os pares:`);
+const pares = numeros.filter((num)=> num % 2 === 0)
+console.log(pares);
+console.log(` `);
+
+console.log(`Dobrando os valores dos pares:`);
+let dobro = pares.map((num)=> num *2)
+console.log(dobro);
+console.log(` `);
+
+console.log(`Somando todos os valores:`);
+let soma = dobro.reduce((acumulador, valor)=> acumulador + valor)
+console.log(soma);
+
+
+
+
