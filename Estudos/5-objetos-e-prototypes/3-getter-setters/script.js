4ee16393b397cb4f3fb85e7370174e995c9f35af
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-12 14:25:43 -0300

    getter e setter em funções

--- Estudos/5-objetos-e-prototypes/3-getter-setters/script.js
+++ Estudos/5-objetos-e-prototypes/3-getter-setters/script.js
@@ -14,15 +14,17 @@ function Produto(nome, preco, estoque) {
     
     // defineProperty:
     Object.defineProperty(this, `estoque`, {
-        enumerable: true, // enumera e mostra a chave
-        configurable: false, // define se  a chave pode ser reconfigurada ou apagada no futura
+        enumerable: true, 
+        configurable: false, 
+
         get : function(){
             return estoquePrivado
         },
+
         set: function(valor){
             if (typeof valor !== `number`) {
-                console.log(`valor invalido`);
-                return
+                throw new TypeError("valor invalido");
+                
             }
             estoquePrivado = valor
             
@@ -41,3 +43,29 @@ p1.estoque = 7
 console.log(p1.estoque);
 
 // cuidado quando for trabalhar com propriedades de objetos, se vc mandar um this.atributo dentro de um setter, causa um loop infinito, se quiser trabalhar com propriedades de um objeto, propriedades privadas
+
+
+
+// função construtora
+
+function  criaProduto(nome, valor, estoque) {
+    let estoquePrivado = estoque;
+    return {
+        nome,
+        valor,
+
+        get estoque(){
+            return estoquePrivado
+        },
+        set estoque(valor){
+            estoquePrivado = valor
+        }
+    }
+}
+
+let p2 = criaProduto(`mesa`, 300, 4)
+console.log(p2.estoque);
+p2.estoque = 2;
+console.log(p2.estoque);
+
+
