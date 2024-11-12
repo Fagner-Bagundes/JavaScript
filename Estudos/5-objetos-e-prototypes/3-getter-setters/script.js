64afd1f5efb12dd7fc81590e8c4561b28d316b44
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-12 14:15:31 -0300

    entendendo getter e setter em objetos

---
+++ Estudos/5-objetos-e-prototypes/3-getter-setters/script.js
@@ -0,0 +1,43 @@
+// get e setter vão além, é uma maneira de proteger uma propriedade, imagine uma propriedade onde voce pode apenas receber numeros. 
+
+// set(setter) --> usado para setar(modificar) o valor, não retorna nada
+// Get(getter) --> usado para obter o valor e validalo
+
+
+// tendo isso, você não precisa usar no defineProperty o value e writable.
+
+
+function Produto(nome, preco, estoque) {
+    this.nome = nome;
+    this.preco = preco;
+    let estoquePrivado = estoque;
+    
+    // defineProperty:
+    Object.defineProperty(this, `estoque`, {
+        enumerable: true, // enumera e mostra a chave
+        configurable: false, // define se  a chave pode ser reconfigurada ou apagada no futura
+        get : function(){
+            return estoquePrivado
+        },
+        set: function(valor){
+            if (typeof valor !== `number`) {
+                console.log(`valor invalido`);
+                return
+            }
+            estoquePrivado = valor
+            
+        }
+
+
+    });
+}
+
+let p1 = new Produto(`Camisa`, 20, 3);
+console.log(p1);
+// para você ver o getter, voce tem que executalo:
+console.log(p1.estoque);
+// setando um valor graças ao setter:
+p1.estoque = 7
+console.log(p1.estoque);
+
+// cuidado quando for trabalhar com propriedades de objetos, se vc mandar um this.atributo dentro de um setter, causa um loop infinito, se quiser trabalhar com propriedades de um objeto, propriedades privadas
