98bbb4f7b9acaedc7383906a723b5496edec0f58
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-16 16:29:20 -0300

    parte de cálculos finalizada

--- desafios/8-validando-CPF/assets/js/script.js
+++ desafios/8-validando-CPF/assets/js/script.js
@@ -1,54 +1,76 @@
 
-let cpf = `064-794-352-22`
-let cpfLimpo = cpf.replace(/\D+/g, ``)
-let cpfArray = Array.from(cpfLimpo);
+let cpf = `370.974.822-49`
+let cpfNumber;
+let cpfMultiplicado1;
+let cpfMultiplicado2;
 
-// Convertendo o cpf em number
-let cpfNumber = cpfArray.map((valor) => {
-    return parseInt(valor)
-})
 
-// Funcão
 
-function multiplicador(params) {
+function TornaCpfCalculavel() {
+    let cpfLimpo = cpf.replace(/\D+/g, ``)
+    let cpfArray = Array.from(cpfLimpo);
+    cpfNumber = cpfArray.map((valor) => parseInt(valor))
+    cpfNumber.splice(-2, 2)
+}
+TornaCpfCalculavel()
+
+// multipllicações:
+
+function multiplicador1(mult1) {
 
     // fazendo a primeira multiplicação
-    let mult1 = 10;
-    let multiplicaCpf1 = cpfNumber.map((valor, indice) => {
-        if (indice <= cpfNumber.length - 3) {
+    let multCpf1 = cpfNumber.map((valor, indice) => {
             let multiplicado = valor * mult1
             --mult1
             return multiplicado
-        }
     })
-    let resultado = multiplicaCpf1.slice(0, -2)
+    cpfMultiplicado1 = multCpf1
+
+}
+
+function resultado1(cpf) {
+    // soma
+    let soma = cpf.reduce((acumulador, valor) => {
+        return acumulador + valor
+    }, 0)
+
+    // divisão
+    return 11 - (soma % 11)
 }
 
 
-let restult1 = mult1.reduce((acumulador, valor) => {
-    return acumulador + valor
-}, 0)
+function multiplicador2(mult) {
+    cpfNumber.push(resultado1(cpfMultiplicado1))
+    // fazendo a primeira multiplicação
+    let multCpf = cpfNumber.map((valor, indice) => {
+            let multiplicado = valor * mult
+            --mult
+            return multiplicado
+    })
+    cpfMultiplicado2 = multCpf
+}
+
+
+function resultado2(cpf) {
+    // soma
+    let soma = cpf.reduce((acumulador, valor) => {
+        return acumulador + valor
+    }, 0)
+
+    // divisão
+    return 11 - (soma % 11)
+}
+
+multiplicador1(10)
+multiplicador2(11)
+let resultados = [resultado1(cpfMultiplicado1),resultado2(cpfMultiplicado2)]
+
+TornaCpfCalculavel()
+const resultadoFinal = [...cpfNumber, ...resultados]
+console.log(resultadoFinal);
 
 
 
 
 
 
-
-
-
-
-
-console.log(mult1);
-console.log(restult1);
-
-
-
-
-
-
-// console.log(geraNum1);
-
-// console.log(cpfArray.reduce((ac, val) =>{
-//     ac + Number(val)
-// }, 0))
