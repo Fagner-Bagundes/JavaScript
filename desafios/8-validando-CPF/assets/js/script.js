4ef35d82495cf0edf6f0cf90813c6faf4c080bc9
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-16 15:28:04 -0300

    fazendo primeiros cálculos para validar um cpf

--- desafios/8-validando-CPF/assets/js/script.js
+++ desafios/8-validando-CPF/assets/js/script.js
@@ -1,14 +1,53 @@
 
-let cpf = `705-484.450-52`
+let cpf = `064-794-352-22`
 let cpfLimpo = cpf.replace(/\D+/g, ``)
 let cpfArray = Array.from(cpfLimpo);
-console.log(cpfArray);
 
-
-let cpfConvertido = cpfArray.map((valor)=>{
+// Convertendo o cpf em number
+let cpfNumber = cpfArray.map((valor) => {
     return parseInt(valor)
 })
-console.log(cpfConvertido);
+
+// Funcão
+
+function multiplicador(params) {
+
+    // fazendo a primeira multiplicação
+    let mult1 = 10;
+    let multiplicaCpf1 = cpfNumber.map((valor, indice) => {
+        if (indice <= cpfNumber.length - 3) {
+            let multiplicado = valor * mult1
+            --mult1
+            return multiplicado
+        }
+    })
+    let resultado = multiplicaCpf1.slice(0, -2)
+}
+
+
+let restult1 = mult1.reduce((acumulador, valor) => {
+    return acumulador + valor
+}, 0)
+
+
+
+
+
+
+
+
+
+
+
+console.log(mult1);
+console.log(restult1);
+
+
+
+
+
+
+// console.log(geraNum1);
 
 // console.log(cpfArray.reduce((ac, val) =>{
 //     ac + Number(val)
