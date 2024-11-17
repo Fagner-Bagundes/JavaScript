a2c8c027c5ffe41277d3219925e1bc7e4acb8aa8
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-17 15:27:12 -0300

    comparando Classes com funções construtoras

--- Estudos/6-programação-POO/1-classes/script.js
+++ Estudos/6-programação-POO/1-classes/script.js
@@ -13,3 +13,24 @@ class Pessoa {
 
 const p1 = new Pessoa(`Fagner`, `Ferreira`)
 console.log(p1);
+
+function Pessoa2(nome, sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+}
+
+Pessoa2.prototype.falar = function (params) {
+    console.log(this.nome, `Está falando oi`)
+}
+Pessoa2.prototype.comer = function (params) {
+    console.log(this.nome, `Está comendo`)
+}
+Pessoa2.prototype.beber = function (params) {
+    console.log(this.nome, `Está Bebendo`)
+}
+Pessoa2.prototype.NCompleto = function (params) {
+    console.log(`${this.nome} ${this.sobrenome}`)
+}
+
+const p2 = new Pessoa2(`Gabriel`, `Imbiriba`);
+console.log(p2);
