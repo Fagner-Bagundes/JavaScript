0cdfb24df5f4a576b2e435d98c3c51f7bcd7219e
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-17 14:22:17 -0300

    fazendo composição e desacoplando o prototype

--- Estudos/5-objetos-e-prototypes/9-factoryFunctions+Prototypes/script.js
+++ Estudos/5-objetos-e-prototypes/9-factoryFunctions+Prototypes/script.js
@@ -1,18 +1,28 @@
-function criaPessoa(nome, sobrenome) {
-    const pessoaPrototye = {
-        falar() {
-            console.log(`${this.nome} está falando`);
-        },
+// Usando o prototype de uma função de fábrica
+const falar = {
+    falar() {
+        console.log(`${this.nome} está falando`);
+    },
+};
 
-        comer(){
-            console.log(`${this.nome} está comendo`);
-        },
-
-        beber(){
-            console.log(`${this.nome} está bebendo`);
-        }
-
+const comer = {
+    comer(){
+        console.log(`${this.nome} está comendo`);
     }
+
+};
+
+const beber = {
+    beber(){
+        console.log(`${this.nome} está bebendo`);
+    }
+};
+
+// composição é quando você cria um objecto concatena vário objetos
+const pessoaPrototye = {...falar, ...comer, ...beber};
+
+function criaPessoa(nome, sobrenome) {
+
     return Object.create(pessoaPrototye, {
         nome: {value: nome},
         sobrenome: {value: sobrenome}
@@ -21,6 +31,7 @@ function criaPessoa(nome, sobrenome) {
 
 const p1 = criaPessoa(`Fagner`, `Ferreira`)
 console.log(p1);
-
+const p2 = criaPessoa(`Vagner`, `Ferreira`)
+console.log(p2);
 
 
