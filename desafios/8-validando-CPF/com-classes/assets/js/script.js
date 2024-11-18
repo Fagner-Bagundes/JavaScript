b73d1aa097b32764b360dbf1933175ce9f416f16
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-18 17:40:41 -0300

    90% do trabalho feito

--- desafios/8-validando-CPF/com-classes/assets/js/script.js
+++ desafios/8-validando-CPF/com-classes/assets/js/script.js
@@ -11,13 +11,24 @@ class ValidaCPF {
     };
 
     geraNovoCpf(){
-        const cpfSemDigitos = this.cpfLimppo.slice(0, -2)
-        const digito1 = this.geraDigitos(cpfSemDigitos);
+        const cpfSemDigitos = this.cpfLimpo.slice(0, -2)
+        const digito1 = this.geraDigito(cpfSemDigitos);
+        const digito2 = this.geraDigito(cpfSemDigitos + digito1)
+        this.novoCpf = this.cpfSemDigitos + digito1 + digito2;
     }
 
     geraDigito(cpfSemdigitos) {
         let total = 0;
-        let reverso = cpfSemdigitos.length + 1
+        let reverso = cpfSemdigitos.length + 1;
+
+        for (let stringNumerica of cpfSemdigitos){
+            // console.log(stringNumerica, reverso);
+            total += reverso * Number(stringNumerica);
+            reverso--
+            
+        }
+        const digito = 11 - (total % 11);
+        return digito <= 9 ? digito : `0`
     }
 
     valida(){
@@ -25,7 +36,8 @@ class ValidaCPF {
         if(typeof this.cpfLimpo !== `string`) return false;
         if(this.cpfLimpo.length !== 11) return false;
         this.geraNovoCpf()
-        return `cheguei aqui` 
+        console.log(this.novoCpf);
+        
     }
 }
 
