048c4c9b0e791c46cd343c6f1cbdf1e796f18539
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-18 11:35:49 -0300

    revisando getters e setters

---
+++ Estudos/6-programação-POO/2-Getters-Setters/script2.js
@@ -0,0 +1,27 @@
+const _Ncompleto = Symbol
+
+class Pessoa {
+    constructor(nome, sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this[_Ncompleto] = ``
+    }
+
+    get nomeCompleto(){
+     this[_Ncompleto] = `${this.nome} ${this.sobrenome}`
+     return this[_Ncompleto]
+    }
+
+    set nomeCompleto(valor){
+        valor = valor.split(` `)
+        this[_Ncompleto] = valor
+    }
+}
+
+const p1 = new Pessoa(`Fagner`, `Ferreira`)
+p1.nomeCompleto = `Vaginaldo Ferreira`
+console.log(p1);
+console.log(` `);
+console.log(p1.nomeCompleto);
+
+
