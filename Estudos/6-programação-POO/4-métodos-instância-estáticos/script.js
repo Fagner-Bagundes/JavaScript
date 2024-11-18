15382a05ac9193b8aa664e6d5a1b90ef546b8306
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-18 16:18:38 -0300

    entendendo melhor métodos estáticos

--- Estudos/6-programação-POO/4-métodos-instância-estáticos/script.js
+++ Estudos/6-programação-POO/4-métodos-instância-estáticos/script.js
@@ -1,11 +1,13 @@
 // Metodos estáticos só podem ser acessados dentro da classea mãe, não podem ser acessadas através de instâncias
 
+// métodos estáticos não podem acessar o this da classe, pois o método não é atribuido a palavra new, que é o objeto do this
+
 class ControleRemoto {
     constructor(tv){
         this.tv = tv;
         this.volume = 0;
     }
-
+    // metodo de instância
     aumentarVolume() {
         this.volume += 2;
     }
@@ -14,9 +16,9 @@ class ControleRemoto {
         this.volume -= 2;
     }
 
-    // Metodo de instância
-    static trocaPilha(){
-        console.log(`Acessado`);
+    // Metodo de Estático
+    static soma(x, y){
+        return x + y
         
     }
 }
@@ -25,7 +27,8 @@ const controle1 = new ControleRemoto(`LG`);
 controle1.aumentarVolume()
 controle1.aumentarVolume()
 console.log(controle1.volume);
-ControleRemoto.trocaPilha();
+console.log(ControleRemoto.soma(10, 20));
+
 
 
 // ControleRemoto.trocaPilha();
