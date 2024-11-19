7a3a48f220bccd6e9d9b81d8e569a728e4d44700
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-19 15:46:20 -0300

    aglumas verificações dos inputs feitas

--- desafios/10-valida-formulario/assets/js/script.js
+++ desafios/10-valida-formulario/assets/js/script.js
@@ -1,25 +1,98 @@
 class ValidaFormulario {
     constructor() {
         this.inputs = document.querySelectorAll(`.input`)
-        this.form = document.querySelector(`.formulario`) 
+        this.form = document.querySelector(`.formulario`)
         this.nome = document.querySelector(`#nome`)
         this.sobrenome = document.querySelector(`#sobrenome`)
-        this.cpf = document.querySelector(`#CPF`) 
+        this.cpf = document.querySelector(`#CPF`)
         this.usuario = document.querySelector(`#usuario`)
         this.senha = document.querySelector(`#senha`)
         this.repSenha = document.querySelector(`#repetir-senha`)
         this.btn = document.querySelector(`#btn`)
-        console.log(this.form);
-        console.log(this.inputs);
-        console.log(this.nome);
-        console.log(this.cpf);
-        console.log(this.usuario);
-        console.log(this.senha);
-        console.log(this.repSenha);
-        
-        
-        
-        
+        this.inicia();
+
+    }
+    inicia() {
+        this.pegaSubmit()
+    }
+
+    naoPreenchidos() {
+        let contador = 0;
+        for (let i = 0; i <= 5; i++) {
+            if (!this.inputs[i].value) {
+                ++contador
+            }
+            if (contador > 5) {
+                this.mostraErros()
+            } else {
+                this.removeErros(true);
+            }
+        }
+    }
+
+    validaNome(){
+        let contemNumESimb = /[0-9!@#\$%\^\&*\)\(+=._-]/.test(this.nome.value)
+
+        if (contemNumESimb) {
+            let nomeError = this.criaDivs(false, true)
+            nomeError.textContent = `Só é permitido letras no nome`
+            nomeError.classList.add(`nomeError`)
+            this.nome.insertAdjacentElement("afterend", nomeError)
+
+            
+        } else{
+            this.removeErros(false, true)
+        }
+    }
+
+    verificaPreenchidos() {
+        this.naoPreenchidos();
+        this.validaNome()
+    }
+
+    criaDivs(erro) {
+        if (erro) {
+            const novaDiv = document.createElement(`div`)
+            novaDiv.style.color = `red`;
+            return novaDiv
+        }
+    }
+
+    mostraErros() {
+        let erros = document.querySelectorAll(`.erros`)
+        for (let i = 0; i <= 5; i++) {
+            if (erros[i]) {
+                erros[i].remove()
+            }
+            this.inputs[i].insertAdjacentElement("afterend", this.criaDivs(true))
+        }
+    }
+
+    removeErros(erros, erro) {
+        if (erros) {
+            let erros = document.querySelectorAll(`.erros`)
+            for (let i = 0; i <= 5; i++) {
+                if (erros[i]) {
+                    erros[i].remove()
+                }
+            }
+        }
+        if (erro) {
+            let nomeErrorInput = document.querySelector(`.nomeError`)
+            if (nomeErrorInput) {
+                nomeErrorInput.remove()
+            }
+        }
+
+    }
+
+   
+
+    pegaSubmit() {
+        this.form.addEventListener(`submit`, (e) => {
+            e.preventDefault();
+            this.verificaPreenchidos();
+        })
     }
 }
 
