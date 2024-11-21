146f19daca4d4ccd828f2d14379ec1cbb26f96c1
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-21 18:57:59 -0300

    IRRF feita

--- Estudos/6-programação-POO/exercicios/classe-funcionario/script.js
+++ Estudos/6-programação-POO/exercicios/classe-funcionario/script.js
@@ -4,12 +4,15 @@ class Funcionario {
         this.cargo = cargo;
         this.salario = salario;
         this.salarioDescINSS;
-        this.salarioDescURRF;
+        this.salarioDescIRRF;
         this.salarioLiquido;
     }
 
     calculaImpostos(){
+      this.INSS()
+      console.log(`Salário com desconto do INNS`);
       
+      this.IRRF()
     }
 
     INSS(){
@@ -18,6 +21,15 @@ class Funcionario {
         if (this.salario >= 2666.69 && this.salario <=4000.03) this.calculaPorcentagemINSS(12, 101.18);
         if (this.salario >= 4000.04 && this.salario <=7786.02) this.calculaPorcentagemINSS(14, 181.18);
     }
+
+    IRRF(){
+
+        if (this.salario >2259.20 && this.salario <=2826.65) this.calculaPorcentagemIRRF(7.5);
+        if (this.salario >= 2866.66 && this.salario <=3751.05) this.calculaPorcentagemIRRF(15);
+        if (this.salario >= 3751.06 && this.salario <=4.664,68) this.calculaPorcentagemIRRF(22.5);
+        
+    }
+
     calculaPorcentagemINSS(porcentagem, deduzir) {
         let desconto = (porcentagem / 100) * this.salario;
         desconto = (desconto * 100) / 100;
@@ -25,16 +37,7 @@ class Funcionario {
         this.salarioDescINSS = (this.salario - desconto).toFixed(2);  
     }
 
-
-    URRF(){
-        if (this.salario <= 1412.00) this.calculaPorcentagemINSS(7.5);
-        if (this.salario > 1412.00 && this.salario <=2666.68) this.calculaPorcentagemINSS(9, 21.18);
-        if (this.salario >= 2666.69 && this.salario <=4000.03) this.calculaPorcentagemINSS(12, 101.18);
-        if (this.salario >= 4000.04 && this.salario <=7786.02) this.calculaPorcentagemINSS(14, 181.18);
-    }
-
-
-    calculaPorcentagemURRF(porcentagem, deduzir) {
+    calculaPorcentagemIRRF(porcentagem, deduzir) {
         let desconto = (porcentagem / 100) * this.salario;
         desconto = (desconto * 100) / 100;
         if (deduzir) desconto -= deduzir
