eb7de9efca018061ccf1dad4c9c830ae4f5956a8
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-21 18:41:08 -0300

    cálculos da porcentagem finalizados

--- Estudos/6-programação-POO/exercicios/classe-funcionario/script.js
+++ Estudos/6-programação-POO/exercicios/classe-funcionario/script.js
@@ -12,36 +12,33 @@ class Funcionario {
       
     }
 
-    calculaPorcentagem(porcentagem, deduzir) {
+    INSS(){
+        if (this.salario <= 1412.00) this.calculaPorcentagemINSS(7.5);
+        if (this.salario > 1412.00 && this.salario <=2666.68) this.calculaPorcentagemINSS(9, 21.18);
+        if (this.salario >= 2666.69 && this.salario <=4000.03) this.calculaPorcentagemINSS(12, 101.18);
+        if (this.salario >= 4000.04 && this.salario <=7786.02) this.calculaPorcentagemINSS(14, 181.18);
+    }
+    calculaPorcentagemINSS(porcentagem, deduzir) {
         let desconto = (porcentagem / 100) * this.salario;
         desconto = (desconto * 100) / 100;
         if (deduzir) desconto -= deduzir
-        
-        console.log(desconto);
         this.salarioDescINSS = (this.salario - desconto).toFixed(2);  
     }
-    INSS(){
-
-        if (this.salario <= 1412.00) {
-            this.calculaPorcentagem(7.5)
-        };
-
-        if (this.salario > 1412.00 && this.salario <=2666.68) {
-            this.calculaPorcentagem(9, 21.18)
-        };
-
-        if (this.salario >= 2666.69 && this.salario <=4000.03) {
-            this.calculaPorcentagem(12)
-        };
-
-        if (this.salario >= 4000.04 && this.salario <=7786.02) {
-            this.calculaPorcentagem(14)
-        };
-
-        
 
 
-        
+    URRF(){
+        if (this.salario <= 1412.00) this.calculaPorcentagemINSS(7.5);
+        if (this.salario > 1412.00 && this.salario <=2666.68) this.calculaPorcentagemINSS(9, 21.18);
+        if (this.salario >= 2666.69 && this.salario <=4000.03) this.calculaPorcentagemINSS(12, 101.18);
+        if (this.salario >= 4000.04 && this.salario <=7786.02) this.calculaPorcentagemINSS(14, 181.18);
+    }
+
+
+    calculaPorcentagemURRF(porcentagem, deduzir) {
+        let desconto = (porcentagem / 100) * this.salario;
+        desconto = (desconto * 100) / 100;
+        if (deduzir) desconto -= deduzir
+        this.salarioDescINSS = (this.salario - desconto).toFixed(2);  
     }
 
     calculaBeneficios(){
@@ -49,6 +46,6 @@ class Funcionario {
     }
 }
 
-const funcionario1 = new Funcionario(`Gabriel Imbiriba`, `Palhaço`, 2666.68) 
+const funcionario1 = new Funcionario(`Gabriel Imbiriba`, `Palhaço`, 1900) 
 
 funcionario1.INSS()
