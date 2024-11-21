76a9f973d3248f434d2b28434c50f7be9d2fc135
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-21 17:09:26 -0300

    começando a fazer a classe Funcionario

---
+++ Estudos/6-programação-POO/exercicios/classe-funcionario/script.js
@@ -0,0 +1,23 @@
+class Funcionario {
+    constructor(nome, cargo, salario){
+        this.nome = nome;
+        this.cargo = cargo;
+        this.salario = salario;
+        this.salarioLiquido;
+    }
+
+    calculaImpostos(){
+        this.salario
+    }
+
+    INSS(){
+
+    }
+
+    calculaBeneficios(){
+
+    }
+}
+
+const funcionario1 = new Funcionario(`Gabriel Imbiriba`, `Palhaço`, 1200) 
+
