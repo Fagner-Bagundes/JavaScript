fbab75ef2822cb32548965d1d13c15efc8eec2d1
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-22 19:19:22 -0300

    criando matrizes e destacando seus valores pares e dizendo quando pares existem

---
+++ Estudos/2-Lógica-de-Programação/praticas/matriz 3x3 par/script.js
@@ -0,0 +1,60 @@
+const input = require('readline-sync');
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+//   });
+const NUMERO_DE_LINHAS = 3
+const NUMERO_DE_COLUNAS = 3
+function cabecalho(params) {
+    console.log(`------------------------------------`);
+    console.log(`           Matriz ${NUMERO_DE_LINHAS}x${NUMERO_DE_COLUNAS}               `);
+    console.log(`------------------------------------`);
+    console.log(`                                    `);
+    
+}
+function rodape() {
+    console.log(`------------------------------------`);
+}
+
+function criarMatriz(nLinhas, nColunas) {
+    let matriz = [];
+    let array = []
+    let numerosPares = [];
+    let contador = 0;
+
+    cabecalho()
+
+    for (let linha = 0; linha < nLinhas; linha++) {
+        for (let coluna = 0; coluna < nColunas; coluna++) {
+                array.push(input.question(`Digite um valor [${linha+ 1}/ ${coluna+1}]:`))
+                if (array[coluna] % 2 ===0) {
+                    numerosPares.push(array[coluna])
+                }
+                contador++
+                if (contador===nColunas) {
+                    matriz.push(array)
+                    array =[]
+                    contador = 0
+                }
+
+        }
+    }
+    console.log(` `);
+    
+    console.log(`Essa é uma matriz ${nLinhas}x${nColunas}`);
+    for (let i = 0; i <nLinhas; i++) {
+        console.log(matriz[i]);
+        
+    }
+    console.log(`numeros pares: ${numerosPares}`);
+    console.log(`toral de numeros pares: ${numerosPares.length}`);
+    
+    rodape();
+
+    
+}
+
+criarMatriz(NUMERO_DE_LINHAS, NUMERO_DE_COLUNAS)
+
+
+
