17c854b7ffeb98fbcb2b50b3bff11826878eba0b
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-23 14:57:23 -0300

    melhorando

--- Estudos/2-Lógica-de-Programação/praticas/invertendo-array/script.js
+++ Estudos/2-Lógica-de-Programação/praticas/invertendo-array/script.js
@@ -1,8 +1,8 @@
 let array = [1,2,3,4,5]
 let arrayinvt = []
 
-let contador = array.length
 function sla(){
+    let contador = array.length
     for(let i of array){
         --contador
         arrayinvt.push(array[contador])
