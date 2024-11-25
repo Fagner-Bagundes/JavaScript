a616102914946ab99eb633b987e796725f293607
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-25 12:53:14 -0300

    entendo alguns metodos das promises

--- Estudos/7-js-assincrono/1-promesses/script.js
+++ Estudos/7-js-assincrono/1-promesses/script.js
@@ -23,37 +23,34 @@
 
 // Uma promise é criada usando o construtor Promise, que recebe uma função de callback com dois argumentos: resolve e reject.
 
-// const minhaPromise = new Promise((resolve, reject) => {
-//   const sucesso = false;
+const minhaPromise = new Promise((resolve, reject) => {
+  const sucesso = false;
 
-//   if (sucesso) {
-//     resolve("A operação foi bem-sucedida!");
-//   } else {
-//     reject("Houve um erro.");
-//   }
-// });
+  if (sucesso) {
+    resolve("A operação foi bem-sucedida!");
+  } else {
+    reject("Houve um erro.");
+  }
+});
 
-
-
-// minhaPromise
-//   .then((resultado) => {
-//     console.log("Sucesso:", resultado);
-//   })
-//   .catch((erro) => {
-//     console.error("Erro:", erro);
-//   })
-//   .finally(() => {
-//     console.log("Operação finalizada.");
-//   });
+minhaPromise
+  .then((resultado) => {
+    console.log("Sucesso:", resultado);
+  })
+  .catch((erro) => {
+    console.error("Erro:", erro);
+  })
+  .finally(() => {
+    console.log("Operação finalizada.");
+    console.log(` `);
+  });
 
 
 //   Aqui está um exemplo mais realista, simulando uma operação assíncrona com setTimeout.
 
-
-
 const buscaDados = new Promise((resolve, reject) => {
     setTimeout(()=>{
-        const dados = { nome: `Fagner`, idade: 19 }
+        const dados = false
         if (dados) {
             resolve(dados)
         }else{
