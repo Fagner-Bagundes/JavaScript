33beb355dd1324ca0d73f0fd349395057c745117
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-25 14:25:14 -0300

    entendo o promise.race()

--- Estudos/7-js-assincrono/2-metodo-uteis/script.js
+++ Estudos/7-js-assincrono/2-metodo-uteis/script.js
@@ -6,7 +6,7 @@ function rand(min, max) {
 
 function esperaAi(msg, tempo){
     return new Promise((resolve, reject)=>{
-        if (typeof msg !== "string") reject(false)
+        if (typeof msg !== "string") reject(`msg invalida`)
         setTimeout(()=>{
             resolve(msg)
         }, tempo)
@@ -22,11 +22,9 @@ function esperaAi(msg, tempo){
 //     com os valores dentro de um array
 
 const promises = [
-    `Primeiro valor`,
-    esperaAi(`promese 1`, 3000),
-    esperaAi(`promese 2`, 500),
-    esperaAi(`promese 3`, 1000),
-    `outro valor`
+    esperaAi(`promese 1`, rand(1,3)),
+    esperaAi(`promese 2`, rand(1,3)),
+    esperaAi(32, rand(1,3)) 
 ];
 
 Promise.all(promises).then(function(valor) {
@@ -35,4 +33,14 @@ Promise.all(promises).then(function(valor) {
     console.log(`Error:`, err);
 })
 
-//
+// Promise.rave:
+// semelhante ao Promise.all, Porem ele retorna apenas um valor, e esse valor é 
+// primeira promise que foi resolvida
+Promise.race(promises).then((resultado)=>{
+    console.log(resultado);
+}).catch((err)=>{
+    console.log(`Error: `, err);
+    
+})
+
+console.log(` `);
