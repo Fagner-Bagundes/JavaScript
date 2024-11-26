9d9839ccd85c2de822753d16ac5f96d236b1460f
Fagner_0 <ferreirabagundes@gmail.com>
2024-11-26 17:33:40 -0300

    manipulando dados recebidos pela API

--- Estudos/7-js-assincrono/4-Fetch-API(GET)/fetch_api_tutorial-main/js/scripts.js
+++ Estudos/7-js-assincrono/4-Fetch-API(GET)/fetch_api_tutorial-main/js/scripts.js
@@ -6,14 +6,28 @@ const postContainer = document.querySelector(`#posts-container`)
 
 // pega todos os posts
 async function getAllPosts() {
-  const response = await fetch(url)
-  const data = await response.json()
+  const response = await fetch(url);
+  const data = await response.json();
   console.log(data);
 
-  loadingElement.classList.add(`hide`)
+  loadingElement.classList.add(`hide`);
   
   data.map((post) =>{
-    
+    const div = document.createElement(`div`);
+    const title = document.createElement(`title`);
+    const body = document.createElement(`p`);
+    const link = document.createElement(`a`);
+
+    title.innerText = post.title;
+    body.innerText = post.body;
+    link.innerText = `Ler`;
+    link.setAttribute("href", `/post.html?id=${post.id}`)
+
+    div.appendChild(title)
+    div.appendChild(body)
+    div.appendChild(link)
+
+    postContainer.appendChild(div)
   })
 };
 
