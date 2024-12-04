0dbbd81fe74f2002d987dcf9b9e946dae5278a02
Fagner_0 <ferreirabagundes@gmail.com>
2024-12-04 14:50:58 -0300

    resolvendo exercício de outra maneira

--- Estudos/7-js-assincrono/3-Async-Await/ativs/loop/script.js
+++ Estudos/7-js-assincrono/3-Async-Await/ativs/loop/script.js
@@ -1,23 +1,43 @@
-function processarArray(indice ,array = [1,2,3,4]) {
-    return new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-                resolve(array[indice])
+// function processarArray(indice ,array = [1,2,3,4]) {
+//     return new Promise((resolve, reject)=>{
+//         setTimeout(()=>{
+//                 resolve(array[indice])
                 
-            }, 1000)
+//             }, 1000)
             
-        }
-    )
+//         }
+//     )
+// }
+
+// async function resolverPromises() {
+
+//     const numero = await processarArray(0)
+//     console.log(numero);
+//     const numer2 = await processarArray(1)
+//     console.log(numer2);
+//     const numer3 = await processarArray(2)
+//     console.log(numer3);
+//     const numer4 = await processarArray(3)
+//     console.log(numer4);
+// }
+
+// resolverPromises()
+
+
+async function ArrayPromise(array) {
+    for(let valor of array){
+        const rand = Math.ceil(Math.random() * (10 -8)) * 1000
+        await esperar(rand)    
+        console.log(valor);
+        
+    }
+    
 }
 
-async function resolverPromises() {
-    const numero = await processarArray(0)
-    console.log(numero);
-    const numer2 = await processarArray(1)
-    console.log(numer2);
-    const numer3 = await processarArray(2)
-    console.log(numer3);
-    const numer4 = await processarArray(3)
-    console.log(numer4);
+
+function esperar(sg) {
+    return new Promise((resoolve)=> setTimeout(resoolve, sg))
 }
 
-resolverPromises()
+
+ArrayPromise([1,2,3,4,5,6])
