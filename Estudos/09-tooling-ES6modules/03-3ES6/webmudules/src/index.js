05ef70fd8a87a2daab4b8743281374967e94146d
Fagner_0 <ferreirabagundes@gmail.com>
2024-12-18 14:46:47 -0300

    entendendo import e export no js

---
+++ Estudos/09-tooling-ES6modules/03-3ES6/webmudules/src/index.js
@@ -0,0 +1,4 @@
+// import * as meuModulo from "./modulo1"
+import algo from "./modulo1"
+
+console.log(algo(5,5));
---
+++ Estudos/09-tooling-ES6modules/03-3ES6/webmudules/src/modulo1.js
@@ -0,0 +1,15 @@
+export const nome = `Luiz`
+export const sobrebrenome = `Mmiranda`
+export const idade = 30;
+
+export default function soma(x,y){
+    return x + y
+}
+
+export class Pessoa {
+    constructor(nome, sob) {
+        this.nome = nome;
+        this.sobrebrenome = sob
+    }
+}
+
