678757b710c2f6bba8c6669846cacb2eeb950e91
Fagner_0 <ferreirabagundes@gmail.com>
2024-12-21 12:34:50 -0300

    fazendo o gerador de simbolos

--- desafios/012-gerador-de-senha/src/modules/GeneratePassword.js
+++ desafios/012-gerador-de-senha/src/modules/GeneratePassword.js
@@ -1,28 +1,36 @@
+
 class ElementsOfPassword{
-    static newNumber(){
-        const numbers = Math.floor(Math.random() * 10)
-        return numbers
-    }
+    static newNumber(){return Math.floor(Math.random() * 10)}
+    
     static newWord(upper){
         const letras = String.fromCharCode(Math.floor(Math.random() * (90 -65) + 65))
         if (upper === `UpperCase`) return  letras.toLocaleUpperCase()
         return letras.toLocaleLowerCase()
     }
+    static newSimbol(){
+        const simbolsArray = [
+            '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/',
+            ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`',
+            '{', '|', '}', '~'
+          ]
+        const simbols = (Math.floor(Math.random() * 31))
+        return simbolsArray[simbols]
+    }
 }
 
 export default class GeneratePassword {
-    constructor(parameters) {
-        
-
+    constructor(){
+        this.limit = (document.querySelector(`#password-lengh`)).value
     }
-
     sla(){
+        console.log(this.limit);
+        
         console.log(ElementsOfPassword.newNumber());
         console.log(ElementsOfPassword.newWord(`UpperCase`));
         console.log(ElementsOfPassword.newWord(`LowerCase`));
+        console.log(ElementsOfPassword.newSimbol());
 
-        
-        
+
         
     }
 
