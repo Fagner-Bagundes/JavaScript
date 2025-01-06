9eaee79115612253ff6d82397798e1534a5bdfc1
FagnerFerreira <ferreirabagundes@gmail.com>
2025-01-05 21:53:35 -0300

    praticando find e findIndex

--- Estudos/04-arrays-avançado/010-find-e-findIndex/script.js
+++ Estudos/04-arrays-avançado/010-find-e-findIndex/script.js
@@ -1,6 +1,44 @@
-let numeros = [1,2,3,5,6]
+// let numeros = [1,2,3,5,6]
 
 
-let maiorQ4 = numeros.find((valor)=> valor >4)
+// let maiorQ4 = numeros.find((valor)=> valor >4)
 
-console.log(maiorQ4);
+// console.log(maiorQ4);
+
+
+let objs = [
+    {
+        nome: "Faganer",
+        endereco: {
+            bairro: `Zerão`, 
+            rua: `Dr.Braulino`,
+            numero: 764
+        }
+    },
+    {
+        nome: "Vagner",
+        endereco: {
+            bairro: `congós`, 
+            rua: `Dr.Braulino`,
+            numero: 354
+        }
+    },
+    {
+        nome: "Magno",
+        endereco: {
+            bairro: `Zerão`, 
+            rua: `Dr.Braulino`,
+            numero: 764
+        }
+    }
+]
+
+
+let pessoa = objs.find((valor)=> valor.nome === "Magno")
+
+let pessoaI = objs.findIndex((valor)=> valor.nome === "Fagner")
+
+
+console.log(pessoa);
+console.log(pessoaI);
+
