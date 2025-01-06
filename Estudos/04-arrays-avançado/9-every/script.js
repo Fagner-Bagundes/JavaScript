3c0e5aca64790aa30de569392c6cb5686456e690
FagnerFerreira <ferreirabagundes@gmail.com>
2025-01-05 21:42:47 -0300

    praticando every()

--- Estudos/04-arrays-avançado/9-every/script.js
+++ Estudos/04-arrays-avançado/9-every/script.js
@@ -1,5 +1,15 @@
-let numeros = [2,5,6,8]
+// let numeros = [2,5,6,8]
 
-let pares = numeros.every((valor)=> valor % 2 === 0)
+// let pares = numeros.every((valor)=> valor % 2 === 0)
 
-console.log(pares);
+// console.log(pares);
+
+let objs = [
+    {nome : `Fagner`, idade: 19},
+    {nome: `vagner`, idade: 29},
+    {nome: `Magno`, idade: 33}
+]
+
+const dMenor = objs.every((obj)=> obj.idade < 18)
+const dMaior = objs.every((obj)=> obj.idade > 18)
+console.log(dMenor, dMaior);
