889715377867cdb0fc9da7e28947df19ad1405d0
FagnerFerreira <ferreirabagundes@gmail.com>
2025-01-08 20:28:57 -0300

    fazendo atividades sobre strings

---
+++ Estudos/praticas/string/ativadades/001.js
@@ -0,0 +1,18 @@
+// // verifique se o valor é uma string
+// function analiseS(valor) {
+//     if (typeof valor !== `string`) {return false}
+//     return true    
+// }
+// console.log(analiseS(`Fagner Ferreira`));
+// console.log(analiseS(23232323));
+
+// verifique se o valor string está vazio
+
+function verifVazio(string) {
+    let valor = !string ? false : true;    
+    return valor
+}
+
+console.log(verifVazio(`fsffsfs`));
+console.log(verifVazio(``));
+
