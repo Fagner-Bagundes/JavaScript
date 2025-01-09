bbb6795aa98ad11001ba956fbc761b48c861a876
FagnerFerreira <ferreirabagundes@gmail.com>
2025-01-08 21:39:48 -0300

    escondendo e-mail do usuario ativ

--- Estudos/praticas/string/ativadades/002.js
+++ Estudos/praticas/string/ativadades/002.js
@@ -1,15 +1,19 @@
+
+// cria array de string
 function createArray(string) {
     return string.split(` `)
 }
 
 console.log(createArray(`Fagner Ferreira Bagundes`));
 
+// ffatia parte da string
 function extraiWord(palavra, num) {
     return palavra.slice(0, num)    
 }
 
 console.log(extraiWord(`Fagner`, 3));
 
+// abrevia nome
 function abrevia(string) {
     let array  = string.split(` `)
     let newArray = array.map((valor, i)=>{
@@ -20,3 +24,15 @@ function abrevia(string) {
 }
 
 console.log(abrevia(`Fagner Ferreira Bagundes`));
+
+// Oculta nome de email
+
+function ocutaEmail(email){
+    let empresa;
+    let nome = email.slice(0, 5)
+    for (let i in email) if (email[i] === `@`) empresa = email.slice(i, email.length)
+    return `${nome}...${empresa}`
+}
+
+console.log(ocutaEmail(`Edinaldoperreira@hotmail`));
+
