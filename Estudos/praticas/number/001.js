d958f69a15934c9d93374a42589de92b6ba8bae8
FagnerFerreira <ferreirabagundes@gmail.com>
2025-01-08 22:43:39 -0300

    fazendo um exercicio com array

--- Estudos/praticas/number/001.js
+++ Estudos/praticas/number/001.js
@@ -12,3 +12,26 @@ function last(array, n){{
 }}
 
 console.log(last([1,2,3,465,665,23,-2]));
+
+// junta arrays:
+function add(array, arrayC) {
+    const newArray = [...array]
+    for(let i in array){
+        if (arrayC[i]) {
+            if (arrayC !== array[i]) {
+                newArray.push(arrayC[i])
+            }
+        }
+    }
+    return newArray
+}
+function union(array1, array2) {
+    
+    if (array1>array2) {
+        return add(array1, array2)
+    }else{
+        return add(array2, array1)
+    }
+}
+
+console.log(union([1,2,3,4], [1,2,7,2,5]));
