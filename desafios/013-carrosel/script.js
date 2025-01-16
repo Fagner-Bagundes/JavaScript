ab58e486e10aca094e4fca094c8d3d3f475d2dfd
FagnerFerreira <ferreirabagundes@gmail.com>
2025-01-16 12:53:21 -0300

    refatorando o código

--- desafios/013-carrosel/script.js
+++ desafios/013-carrosel/script.js
@@ -1,6 +1,7 @@
 const postersContainer = document.querySelector(`.posters-container`)
 const avancarb = document.querySelector(`#avancar`)
 const voltarb = document.querySelector(`#voltar`)
+const selects = document.querySelectorAll(`.select`)
 
 const imgs = [
     {id: `id1`, url: `./imgs/berserk.png`},
@@ -11,27 +12,38 @@ const imgs = [
     {id: `id6`, url: `./imgs/vinland.jpg`}
 
 ]
+addPosts()
 
 function addPosts() {
-    imgs.forEach((img)=>{
+    imgs.forEach((img, i)=>{
         postersContainer.innerHTML += `
            <img class="poster" id="${img.id}" src="${img.url}" alt="">
-        `
+        `;
+        selects[i].id = `${img.id}`
+    })
+    postersContainer.firstElementChild.classList.add(`first`)
+}
+
+
+const atualiza = ()=> document.querySelectorAll(`.poster`)
+
+function btsActive(pos) { 
+    atualiza().forEach((post, i)=>{
+        if (post.classList.contains(`first`)) return selects[i].classList.add(`ativ`)
+            selects[i].classList.remove(`ativ`)
     })
 }
 
-addPosts()
-postersContainer.firstElementChild.classList.add(`first`)
-
 voltarb.addEventListener(`click`,(e)=>{
-    const posters = document.querySelectorAll(`.poster`)
-    postersContainer.appendChild(posters[0])
-    
+    postersContainer.appendChild(atualiza()[0])
+    btsActive(atualiza)    
 })
 
 avancarb.addEventListener(`click`, (e)=>{
     const ultimo = postersContainer.lastElementChild
     postersContainer.prepend(ultimo)
+    btsActive()
 })
+
 
 
