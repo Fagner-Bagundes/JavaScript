e6ca3370f702c422d6718910eb48a5640c3bc110
FagnerFerreira <ferreirabagundes@gmail.com>
2025-01-15 22:36:10 -0300

    botao voltar finalizado

--- desafios/013-carrosel/script.js
+++ desafios/013-carrosel/script.js
@@ -1,14 +1,18 @@
 const postersContainer = document.querySelector(`.posters-container`)
+const avancarb = document.querySelector(`#avancarb`)
+const voltarb = document.querySelector(`#voltarb`)
+
+
 
 console.log(postersContainer);
 
 const imgs = [
-    {id: 1, url: `./imgs/berserk.png`},
-    {id: 2, url: `./imgs/bleach.png`},
-    {id: 3, url: `./imgs/jojo.jpg`},
-    {id: 4, url: `./imgs/one-piece.jpg`},
-    {id: 5, url: `./imgs/vagabond.jpg`},
-    {id: 6, url: `./imgs/vinland.jpg`}
+    {id: `id1`, url: `./imgs/berserk.png`},
+    {id: `id2`, url: `./imgs/bleach.png`},
+    {id: `id3`, url: `./imgs/jojo.jpg`},
+    {id: `id4`, url: `./imgs/one-piece.jpg`},
+    {id: `id5`, url: `./imgs/vagabond.jpg`},
+    {id: `id6`, url: `./imgs/vinland.jpg`}
 
 ]
 
@@ -21,3 +25,19 @@ function addPosts() {
 }
 
 addPosts()
+postersContainer.firstElementChild.classList.add(`first`)
+voltarb.addEventListener(`click`,(e)=>{
+    const posters = document.querySelectorAll(`.poster`)
+    postersContainer.appendChild(posters[0])
+    postersContainer.firstElementChild.classList.add(`first`)
+    postersContainer.lastElementChild.classList.remove(`first`)
+    
+})
+
+
+
+// posters.forEach((post)=>{
+    
+    
+// })
+
